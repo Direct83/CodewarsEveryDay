95b1a2d64c85a2a1aec3ad465f021eecbc350f9b
Ivan Vidutin <Forumuse@yandex.ru>
2021-10-02 10:27:53 +0500

    02102021

--- codewars.js
+++ codewars.js
@@ -1,5 +1,26 @@
-function sumOfMinimums(arr) {
-	return arr.map(el => Math.min(...el)).reduce((a, b) => a + b)
+const alphabetWar = (fight) => {
+	const left ={
+		w: 4,
+		p: 3,
+		b: 2,
+		s: 1,
+		m: -4,
+		q: -3,
+		d: -2,
+		z: -1
+	}
+
+	const power = [...fight].reduce((acc, el) => {
+		acc += left[el] || 0 + right[el] || 0
+		return acc
+	}, 0)
+
+	if (power === 0) {
+		return "Let's fight again!"
+	}
+
+	return  power > 0 ? "Left side wins!" : "Right side wins!"
 }
 
-console.log(sumOfMinimums([[7, 9, 8, 6, 2], [6, 3, 5, 4, 3], [5, 8, 7, 4, 5]]))
+console.log(alphabetWar("z"))
+console.log(alphabetWar("zdqmwpbs"))
--- completedTasks.js
+++ completedTasks.js
@@ -19,3 +19,40 @@
 // }
 
 // console.log(sumOfMinimums([[7, 9, 8, 6, 2], [6, 3, 5, 4, 3], [5, 8, 7, 4, 5]]))
+
+// const isSortedAndHow = array => {
+// 	const descending = array.filter((el, index, arr) => el > arr[index + 1]).length === 0
+// 	const ascending = array.filter((el, index, arr) => el < arr[index + 1]).length === 0
+// 	return ascending ? 'yes, ascending' : descending ? 'yes, descending' : 'no'
+// }
+
+// console.log(isSortedAndHow([1, 2, 3, 4, 5]))
+// console.log(isSortedAndHow([5, 4, 3, 2, 1]))
+// console.log(isSortedAndHow([4, 3, 30]))
+
+// const alphabetWar = (fight) => {
+// 	const left ={
+// 		w: 4,
+// 		p: 3,
+// 		b: 2,
+// 		s: 1,
+// 		m: -4,
+// 		q: -3,
+// 		d: -2,
+// 		z: -1
+// 	}
+
+// 	const power = [...fight].reduce((acc, el) => {
+// 		acc += left[el] || 0 + right[el] || 0
+// 		return acc
+// 	}, 0)
+
+// 	if (power === 0) {
+// 		return "Let's fight again!"
+// 	}
+
+// 	return  power > 0 ? "Left side wins!" : "Right side wins!"
+// }
+
+// console.log(alphabetWar("z"))
+// console.log(alphabetWar("zdqmwpbs"))
