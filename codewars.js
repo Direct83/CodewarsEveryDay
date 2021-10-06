e97a0a33dd6ad858640357f30ccbc5e7cdf00dde
Ivan Vidutin <Forumuse@yandex.ru>
2021-10-06 22:03:21 +0500

    06102021

--- codewars.js
+++ codewars.js
@@ -1,3 +1,1 @@
-const wordsToMarks = (str) => [...str].reduce((acc,el) => acc += el.charCodeAt()-96, 0)
-
-console.log(wordsToMarks('ab'))
+const removeExclamationMarks = s => s.replace(/!/g, '');
--- completedTasks.js
+++ completedTasks.js
@@ -113,3 +113,5 @@
 // const wordsToMarks = (str) => [...str].reduce((acc,el) => acc += el.charCodeAt()-96, 0)
 
 // console.log(wordsToMarks('ab'))
+
+// const removeExclamationMarks = s => s.replace(/!/g, '');
