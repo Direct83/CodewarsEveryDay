c1a28dd47f6cce5a5a02ff381b4ed5259c107ccd
Ivan Vidutin <Forumuse@yandex.ru>
2021-10-07 22:42:37 +0500

    07102021

--- codewars.js
+++ codewars.js
@@ -1,1 +1,10 @@
-const removeExclamationMarks = s => s.replace(/!/g, '');
+function sumTriangularNumbers(n) {
+	if (n < 0) {
+		return 0
+	}
+
+	const sum = sumTriangularNumbers(n-1)
+	return sum + (n * (n + 1)) / 2
+}
+
+console.log(sumTriangularNumbers(6))
--- completedTasks.js
+++ completedTasks.js
@@ -115,3 +115,14 @@
 // console.log(wordsToMarks('ab'))
 
 // const removeExclamationMarks = s => s.replace(/!/g, '');
+
+// function sumTriangularNumbers(n) {
+// 	if (n < 0) {
+// 		return 0
+// 	}
+
+// 	const sum = sumTriangularNumbers(n-1)
+// 	return sum + (n * (n + 1)) / 2
+// }
+
+// console.log(sumTriangularNumbers(6))
