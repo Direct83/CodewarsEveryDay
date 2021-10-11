3b9a8c5203e874887245e3b5bad4d0a46e0fb8b7
Ivan Vidutin <Forumuse@yandex.ru>
2021-10-11 10:50:15 +0500

    11102021

--- completedTasks.js
+++ completedTasks.js
@@ -147,3 +147,13 @@
 // const halvingSum = (n) => n ? n + halvingSum(Math.floor(n/2)) : 0
 
 // console.log(halvingSum(25))
+
+// function menFromBoys(arr){
+// 	const arrUniq = [...new Set(arr)]
+// 	const even = arrUniq.filter(el => el % 2 === 0).sort((a, b) => a - b)
+// 	const odds = arrUniq.filter(el => el % 2).sort((a, b) => b - a)
+// 	return [...even, ...odds]
+// }
+
+// console.log(menFromBoys([2,43,95,90,37]))
+// console.log(menFromBoys([2,15,17,15,2,10,10,17,1,1]))
