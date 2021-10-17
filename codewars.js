dc1c2844857a633f39c098e285a12ac2a0d35aa5
Ivan Vidutin <Forumuse@yandex.ru>
2021-10-17 19:08:58 +0500

    17102021

--- codewars.js
+++ codewars.js
@@ -1,3 +1,10 @@
-const maxNumber = (n) => Number([...String(n)].sort((a, b) => b - a).join(''))
-
-console.log(maxNumber(123))  // 321
+function adjacentElementsProduct(array) {
+	return array.reduce((acc, el, i, m) => {
+		const number = m[i + 1] * el
+		acc = number > acc ? number : acc
+		return acc
+	}, array[0] * array[1])
+}
+console.log(adjacentElementsProduct([5, 8])) // 40
+console.log(adjacentElementsProduct([-23, 4, -5, 99, -27, 329, -2, 7, -921])) // -14
+console.log(adjacentElementsProduct([ -714, 682 ])) //-486948
