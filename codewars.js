f8a8995a1c6c7339d01c8521700a23c9392b3db7
Ivan Vidutin <Forumuse@yandex.ru>
2021-10-28 10:42:31 +0500

    28102021

--- codewars.js
+++ codewars.js
@@ -1,3 +1,12 @@
-const sumCubes = (n) => n === 1? 1: n**3 + sumCubes(n - 1)
+const evenNumbers = (array, number) => array.filter((el) => el % 2 === 0).slice(-number)
 
-console.log(sumCubes(3))// 3 --> 36 (sum of the cubes of 1, 2, and 3 is 1 + 8 + 27)
+console.log(evenNumbers([1, 2, 3, 4, 5, 6, 7, 8, 9], 3)) // [4, 6, 8]
+
+const str = 'fdggsGFfgferhjg78d6fr86dg4r8g4'
+
+const fun = (str) => {
+	return {
+		str: str.match(/[a-z]/gi).filter((el, i, m) => m.lastIndexOf(el) === i),
+		numbers: str.match(/\d/g).reduce((acc, el) => acc + Number(el), 0)
+	}
+}
