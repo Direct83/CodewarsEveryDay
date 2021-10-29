d9509bbebbad7fde64283146d9473741950783d7
Ivan Vidutin <Forumuse@yandex.ru>
2021-10-29 08:39:24 +0500

    29102021

--- codewars.js
+++ codewars.js
@@ -1,12 +1,9 @@
-const evenNumbers = (array, number) => array.filter((el) => el % 2 === 0).slice(-number)
-
-console.log(evenNumbers([1, 2, 3, 4, 5, 6, 7, 8, 9], 3)) // [4, 6, 8]
-
-const str = 'fdggsGFfgferhjg78d6fr86dg4r8g4'
-
-const fun = (str) => {
-	return {
-		str: str.match(/[a-z]/gi).filter((el, i, m) => m.lastIndexOf(el) === i),
-		numbers: str.match(/\d/g).reduce((acc, el) => acc + Number(el), 0)
-	}
+const vowelIndices = (word) => {
+	const vowel =['a', 'e', 'i', 'o', 'u', 'y']
+	return [...word].reduce((acc, el, i) => {
+		vowel.includes(el) && acc.push(i + 1)
+		return acc
+	}, [])
 }
+
+console.log(vowelIndices("apple")) // [1,5]
--- completedTasks.js
+++ completedTasks.js
@@ -292,3 +292,13 @@
 // }
 
 // console.log(fun(str))
+
+// const vowelIndices = (word) => {
+// 	const vowel =['a', 'e', 'i', 'o', 'u', 'y']
+// 	return [...word].reduce((acc, el, i) => {
+// 		vowel.includes(el) && acc.push(i + 1)
+// 		return acc
+// 	}, [])
+// }
+
+// console.log(vowelIndices("apple")) // [1,5]
