1a9f0acb5bc64278f657080d518ce2672573e6c9
Ivan Vidutin <Forumuse@yandex.ru>
2021-11-02 11:03:15 +0500

    02112021

--- codewars.js
+++ codewars.js
@@ -1,4 +1,13 @@
-const multiplyAll = (arr) => (n) => arr.map((x) => n * x);
+function cubeOdd(arr) {
+	if (arr.find((el) => typeof el !== 'number')) {
+		return undefined;
+	}
+	return arr.reduce((acc, el, i) => {
+		const number = el ** 3;
+		acc = number % 2 !== 0 ? acc + number : acc;
+		return acc;
+	}, 0);
+}
 
-console.log(multiplyAll([1, 2, 3])(1)); // [1, 2, 3]
-console.log(multiplyAll([1, 2, 3])(2)); // [2, 4, 6]
+console.log(cubeOdd([1, 2, 3, 4])); // 28
+console.log(cubeOdd([-3, -2, 2, 3]));
--- completedTasks.js
+++ completedTasks.js
@@ -311,3 +311,17 @@
 
 // console.log(multiplyAll([1, 2, 3])(1)); // [1, 2, 3]
 // console.log(multiplyAll([1, 2, 3])(2)); // [2, 4, 6]
+
+// function cubeOdd(arr) {
+// 	if (arr.find((el) => typeof el !== 'number')) {
+// 		return undefined;
+// 	}
+// 	return arr.reduce((acc, el, i) => {
+// 		const number = el ** 3;
+// 		acc = number % 2 !== 0 ? acc + number : acc;
+// 		return acc;
+// 	}, 0);
+// }
+
+// console.log(cubeOdd([1, 2, 3, 4])); // 28
+// console.log(cubeOdd([-3, -2, 2, 3]));
