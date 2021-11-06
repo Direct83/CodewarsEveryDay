31601e5f7c75ccc06029e45428a5d4235e3af9b5
Ivan Vidutin <Forumuse@yandex.ru>
2021-11-06 11:25:01 +0500

    06112021

--- codewars.js
+++ codewars.js
@@ -1,9 +1,7 @@
-const strong = (n) =>
-	[...String(n)].reduce((acc, el) => {
-		acc = acc + [...Array(Number(el))].reduce((acc, _, i) => acc * (i + 1), 1);
-		return acc;
-	}, 0) === n
-		? 'STRONG!!!!'
-		: 'Not Strong !!';
+function add(n) {
+	return function Add(b) {
+		return n + b;
+	};
+}
 
-console.log(strong(145)); // 'STRONG!!!!'
+console.log(add(1)(3)); // 4
--- completedTasks.js
+++ completedTasks.js
@@ -362,3 +362,11 @@
 // 		: 'Not Strong !!';
 
 // console.log(strong(145)); // 'STRONG!!!!'
+
+// function add(n) {
+// 	return function Add(b) {
+// 		return n + b;
+// 	};
+// }
+
+// console.log(add(1)(3)) // 4
