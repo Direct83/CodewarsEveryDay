c90076cffef526af7eb3f1e82423d560d67ee7ea
Ivan Vidutin <Forumuse@yandex.ru>
2021-11-07 06:40:24 +0500

    07112021

--- codewars.js
+++ codewars.js
@@ -1,7 +1,13 @@
-function add(n) {
-	return function Add(b) {
-		return n + b;
-	};
+function add(a, b) {
+	let str = '';
+	while (a || b) {
+		str = (a % 10) + (b % 10) + str;
+		a = Math.floor(a / 10);
+		b = Math.floor(b / 10);
+	}
+	return +str;
 }
 
-console.log(add(1)(3)); // 4
+console.log(add(11, 99)); // 1010
+console.log(add(122, 81)); // 1103
+console.log(add(38810, 1383)); // 391193
--- completedTasks.js
+++ completedTasks.js
@@ -370,3 +370,17 @@
 // }
 
 // console.log(add(1)(3)) // 4
+
+// function add(a, b) {
+// 	let str = '';
+// 	while (a || b) {
+// 		str = (a % 10) + (b % 10) + str;
+// 		a = Math.floor(a / 10);
+// 		b = Math.floor(b / 10);
+// 	}
+// 	return +str;
+// }
+
+// console.log(add(11, 99)); // 1010
+// console.log(add(122, 81)); // 1103
+// console.log(add(38810, 1383)); // 391193
