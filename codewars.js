109e329788aa40d16fb0fd69adde2cab9ce07e3e
Ivan Vidutin <Forumuse@yandex.ru>
2021-11-13 14:47:30 +0500

    13112021

--- codewars.js
+++ codewars.js
@@ -1,5 +1,18 @@
-const greet = (name) =>
-	`Hello ${name[0].toUpperCase() + name.slice(1).toLowerCase()}!`;
+const streetFighterSelection = (fighters, [i, j], moves) => {
+	return moves.map((move) => {
+		if (move === 'up') i = Math.max(i - 1, 0);
+		if (move === 'down') i = Math.min(i + 1, 1);
+		if (move === 'left') j = (j - 1 + 6) % 6;
+		if (move === 'right') j = (j + 1 + 6) % 6;
 
-console.log(greet('riley')); // 'Hello Riley!'
-console.log(greet('BILLY')); // 'Hello Billy!'
+		return fighters[i][j];
+	});
+};
+
+const fighters = [
+	['Ryu', 'E.Honda', 'Blanka', 'Guile', 'Balrog', 'Vega'],
+	['Ken', 'Chun Li', 'Zangief', 'Dhalsim', 'Sagat', 'M.Bison'],
+];
+
+const moves = ['up', 'left', 'right', 'left', 'left'];
+console.log(streetFighterSelection(fighters, [0, 0], moves));
