4b946a4f52e735abd6289b6e7f71ae6f590bc9cc
Ivan Vidutin <Forumuse@yandex.ru>
2021-11-16 09:46:50 +0500

    16112021

--- codewars.js
+++ codewars.js
@@ -1,12 +1,7 @@
-const wordValue = (arr) =>
-	arr.reduce((acc, el, i) => {
-		const words = el.match(/[a-z]/gi);
-		const countWords = words
-			? words.reduce((acc, el) => acc + el.charCodeAt() - 96, 0)
-			: 0;
-		return acc.concat(countWords * (i + 1));
-	}, []);
+const encode = (str, num) =>
+	[...str].map(
+		(el, i) =>
+			el.charCodeAt(0) - 96 + Number(String(num)[i % String(num).length])
+	);
 
-console.log(wordValue(['codewars', 'abc', 'xyz'])); // [88,12,225]
-console.log(wordValue(['abc abc', 'abc abc', 'abc', 'abc', ' '])); // [12,24,18,24,0]
-console.log(wordValue(['']));
+console.log(encode('scout', 1939)); // [ 20, 12, 18, 30, 21]
--- completedTasks.js
+++ completedTasks.js
@@ -441,3 +441,11 @@
 
 // const moves = ['up', 'left', 'right', 'left', 'left'];
 // console.log(streetFighterSelection(fighters, [0, 0], moves));
+
+// const encode = (str, num) =>
+// 	[...str].map(
+// 		(el, i) =>
+// 			el.charCodeAt(0) - 96 + Number(String(num)[i % String(num).length])
+// 	);
+
+// console.log(encode('scout', 1939)); // [ 20, 12, 18, 30, 21]
