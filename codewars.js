66d305d3490acbd7d01edfff6c2e65267000e3ac
Ivan Vidutin <Forumuse@yandex.ru>
2021-11-17 23:15:06 +0500

    17112021

--- codewars.js
+++ codewars.js
@@ -1,7 +1,11 @@
-const encode = (str, num) =>
-	[...str].map(
-		(el, i) =>
-			el.charCodeAt(0) - 96 + Number(String(num)[i % String(num).length])
-	);
+function even_or_odd(number) {
+	if (number % 2 == 0) {
+		return 'Even';
+	}
+	return 'Odd';
+}
 
-console.log(encode('scout', 1939)); // [ 20, 12, 18, 30, 21]
+console.log(even_or_odd(2), 'Even');
+console.log(even_or_odd(0), 'Even');
+console.log(even_or_odd(7), 'Odd');
+console.log(even_or_odd(1), 'Odd');
