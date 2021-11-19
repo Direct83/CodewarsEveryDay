3fc3be625c9f2c92591d1701cacac83881464369
Ivan Vidutin <Forumuse@yandex.ru>
2021-11-19 08:51:33 +0500

    19112021

--- codewars.js
+++ codewars.js
@@ -1,4 +1,14 @@
-const helloWorld = () => {
-	var str = 'Hello World!';
-	console.log(str);
-};
+const sum = (firstNumber, result = firstNumber) =>
+	(next = (secondNumber) => {
+		if (secondNumber !== undefined) {
+			result += secondNumber;
+			return next;
+		}
+		return result;
+	});
+
+console.log(sum(1)()); // 1
+console.log(sum(1)(2)()); // 3
+console.log(sum(1)(2)(3)()); // 6
+console.log(sum(1)(2)(3)(4)()); // 10
+console.log(sum(1)(2)(3)(4)(5)()); // 15
