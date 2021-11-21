8736a6b46535995a8cd39058904ecc64c68fcf80
Ivan Vidutin <Forumuse@yandex.ru>
2021-11-21 09:01:59 +0500

    21112021

--- completedTasks.js
+++ completedTasks.js
@@ -482,3 +482,19 @@
 // console.log(sum(1)(2)(3)(4)(5)()); // 15
 
 // const getEvenNumbers = numbersArray => numbersArray.filter(a => a % 2 === 0);
+
+// function prime(num) {
+// 	return [...Array(num)].reduce((acc, el, i) => {
+// 		const number = i + 1;
+// 		return acc.concat((isPrime(number) && number) || []);
+// 	}, []);
+// }
+
+// function isPrime(number) {
+// 	for (let i = 2; i <= Math.sqrt(number); i++) {
+// 		if (number % i === 0) return false;
+// 	}
+// 	return number !== 1;
+// }
+
+// console.log(prime(23)); // [2, 3, 5, 7, 11, 13, 17, 19, 23];
