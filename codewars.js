21726470c14ab4995d49b5839b02fd05720cbec2
Ivan Vidutin <Forumuse@yandex.ru>
2021-11-24 10:58:42 +0500

    24112021

--- codewars.js
+++ codewars.js
@@ -1,4 +1,5 @@
-const countRedBeads = (n) => (n ? (n - 1) * 2 : n);
+const findDeletedNumber = (arr, mixArr) =>
+	arr.find((el) => !mixArr.includes(el)) || 0;
 
-console.log(countRedBeads(0)); // 0
-console.log(countRedBeads(5)); // 8
+console.log(findDeletedNumber([1, 2, 3, 4, 5], [3, 4, 1, 5])); // 2
+console.log(findDeletedNumber([1, 2, 3, 4, 5], [1, 2, 3, 4, 5])); // 0
--- completedTasks.js
+++ completedTasks.js
@@ -508,3 +508,9 @@
 
 // console.log(countRedBeads(0)); // 0
 // console.log(countRedBeads(5)); // 8
+
+// const findDeletedNumber = (arr, mixArr) =>
+// 	arr.find((el) => !mixArr.includes(el)) || 0;
+
+// console.log(findDeletedNumber([1, 2, 3, 4, 5], [3, 4, 1, 5])); // 2
+// console.log(findDeletedNumber([1, 2, 3, 4, 5], [1, 2, 3, 4, 5])); // 0
