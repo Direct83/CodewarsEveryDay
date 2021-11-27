79d1dd3d3bfd091847765e8d4f5719d298b8e288
Ivan Vidutin <Forumuse@yandex.ru>
2021-11-27 14:14:38 +0500

    27112021

--- codewars.js
+++ codewars.js
@@ -1,9 +1,19 @@
-const alternateCase = (string) =>
-	[...string].reduce((acc, el) => {
-		const alternateCase =
-			el.toUpperCase() === el ? el.toLowerCase() : el.toUpperCase();
-		return acc + alternateCase;
-	}, '');
+function calculate(num1, operation, num2) {
+	if (/\w/.test(operation) || (/\//.test(operation) && num2 === 0)) {
+		return null;
+	}
+	switch (operation) {
+		case '+':
+			return num1 + num2;
+		case '-':
+			return num1 - num2;
+		case '/':
+			return num1 / num2;
+		case '*':
+			return num1 * num2;
+	}
+}
 
-console.log(alternateCase('abc'));
-console.log(alternateCase('ABC'));
+console.log(calculate(3.2, '+', 8));
+console.log(calculate(-3, '/', 0));
+console.log(calculate(-3, 'm', 0));
--- completedTasks.js
+++ completedTasks.js
@@ -538,3 +538,23 @@
 
 // console.log(alternateCase('abc'));
 // console.log(alternateCase('ABC'));
+
+// function calculate(num1, operation, num2) {
+// 	if (/\w/.test(operation) || (/\//.test(operation) && num2 === 0)) {
+// 		return null;
+// 	}
+// 	switch (operation) {
+// 		case '+':
+// 			return num1 + num2;
+// 		case '-':
+// 			return num1 - num2;
+// 		case '/':
+// 			return num1 / num2;
+// 		case '*':
+// 			return num1 * num2;
+// 	}
+// }
+
+// console.log(calculate(3.2, '+', 8));
+// console.log(calculate(-3, '/', 0));
+// console.log(calculate(-3, 'm', 0));
