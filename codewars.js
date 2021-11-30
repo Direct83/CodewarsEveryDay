ffd082de3c3f6ae78e7236860ea89d7afe1ed2e7
Ivan Vidutin <Forumuse@yandex.ru>
2021-11-30 08:22:04 +0500

    30112021

--- codewars.js
+++ codewars.js
@@ -1,4 +1,5 @@
-const hasUniqueChars = (str) => new Set(str).size === str.length;
+const maxDiff = (list) =>
+	list.length ? Math.max(...list) - Math.min(...list) : 0;
 
-console.log(hasUniqueChars('  nAa')); // false
-console.log(hasUniqueChars('abcdef')); // true
+console.log(maxDiff([0, 1, 2, 3, 4, 5, 6])); // 6
+console.log(maxDiff([-0, 1, 2, -3, 4, 5, -6])); // 11
--- completedTasks.js
+++ completedTasks.js
@@ -571,3 +571,9 @@
 
 // console.log(hasUniqueChars('  nAa')); // false
 // console.log(hasUniqueChars("abcdef")) // true
+
+// const maxDiff = (list) =>
+// 	list.length ? Math.max(...list) - Math.min(...list) : 0;
+
+// console.log(maxDiff([0, 1, 2, 3, 4, 5, 6])); // 6
+// console.log(maxDiff([-0, 1, 2, -3, 4, 5, -6])) // 11
