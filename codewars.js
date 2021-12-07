3200562ab2ce3665f3511f03c31694df9e3da6e7
Ivan Vidutin <Forumuse@yandex.ru>
2021-12-07 10:05:07 +0500

    07122021

--- codewars.js
+++ codewars.js
@@ -1,3 +1,18 @@
-const hasSubpattern = (string) => /^(.*)\1+$/.test(string);
-
-console.log(hasSubpattern('abbaabbaabba')); // true
+function meeting(s) {
+	const arr = s.toUpperCase().split(/:|;/g);
+	return [...Array(arr.length / 2)]
+		.map((el) => arr.splice(0, 2).reverse())
+		.sort()
+		.map((el) => `(${el[0]}, ${el[1]})`)
+		.join('');
+}
+console.log(
+	meeting(
+		'Alexis:Wahl;John:Bell;Victoria:Schwarz;Abba:Dorny;Grace:Meta;Ann:Arno;Madison:STAN;Alex:Cornwell;Lewis:Kern;Megan:Stan;Alex:Korn'
+	)
+);
+console.log(
+	meeting(
+		'Fred:Corwill;Wilfred:Corwill;Barney:Tornbull;Betty:Tornbull;Bjon:Tornbull;Raphael:Corwill;Alfred:Corwill'
+	)
+);
