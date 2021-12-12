bb907a402dc55bb69e5752885fd4c852a2704a80
Ivan Vidutin <Forumuse@yandex.ru>
2021-12-12 16:42:45 +0500

    12122021

--- codewars.js
+++ codewars.js
@@ -1,16 +1,9 @@
-const revrot = (str, sz) =>
-	sz <= 0 || sz > str.length || str == ''
-		? ''
-		: str2chunks(str, sz)
-				.map((chunk) =>
-					isSumOfCubesOfDigitsOdd(chunk) ? rotate(chunk) : reverse(chunk)
-				)
-				.join('');
+const arrayLeaders = (numbers) =>
+	numbers.filter((number, index) => {
+		const rightNumbers = numbers
+			.slice(index + 1)
+			.reduce((total, number) => total + number, 0);
+		return number > rightNumbers;
+	});
 
-const str2chunks = (s, c) => s.match(new RegExp('.{' + c + '}', 'g')) || [];
-const isSumOfCubesOfDigitsOdd = (str) => str.replace(/[02468]/g, '').length % 2;
-const rotate = (str) => str.slice(1) + str[0];
-const reverse = (str) => str.split('').reverse().join('');
-
-console.log(revrot('733049910872815764', 5)); // 330479108928157
-console.log(revrot('73304991087281576455176044327690580265896', 8)); // 1994033775182780067155464327690480265895
+console.log(arrayLeaders([16, 17, 4, 3, 5, 2])); // [17,5,2]
--- completedTasks.js
+++ completedTasks.js
@@ -700,3 +700,13 @@
 
 // console.log(revrot('733049910872815764', 5)); // 330479108928157
 // console.log(revrot('73304991087281576455176044327690580265896', 8)); // 1994033775182780067155464327690480265895
+
+// const arrayLeaders = (numbers) =>
+// 	numbers.filter((number, index) => {
+// 		const rightNumbers = numbers
+// 			.slice(index + 1)
+// 			.reduce((total, number) => total + number, 0);
+// 		return number > rightNumbers;
+// 	});
+
+// console.log(arrayLeaders([16, 17, 4, 3, 5, 2])); // [17,5,2]
