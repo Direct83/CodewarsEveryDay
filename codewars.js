1c2c3424d40607c63adcbe1f219ac9bf133501cb
Ivan Vidutin <Forumuse@yandex.ru>
2021-12-15 08:54:25 +0500

    15122021

--- codewars.js
+++ codewars.js
@@ -1,23 +1,59 @@
-// ёлочка
-const tree = (symbol, height) =>
-	[...Array(height)]
-		.map(
-			(_, index) =>
-				' '.repeat(height - index - 1) +
-				symbol.repeat((index + 1) * 2 - 1) +
-				' '.repeat(height - index - 1)
-		)
-		.join('\n');
+const scale = (str, numberRepetitions, quantity) =>
+	!str
+		? str
+		: str
+				.split('\n')
+				.map((word) =>
+					[...Array(quantity)]
+						.map((_) =>
+							[...word].map((el) => el.repeat(numberRepetitions)).join('')
+						)
+						.join('\n')
+				)
+				.join('\n');
 
-console.log(tree('8', 5));
-//    8
-//   888
-//  88888
-// 8888888
-//888888888
+const scale = (str, numberRepetitions, quantity) => {
+	if (str === '') {
+		return '';
+	}
+	const words = str.split('\n');
+	const resultArr = [];
+	for (let i = 0; i < words.length; i++) {
+		const innerWords = words[i].split('');
+		const resultInnerWords = [];
+		for (let i = 0; i < innerWords.length; i++) {
+			resultInnerWords.push(innerWords[i].repeat(numberRepetitions));
+		}
+		resultArr.push((resultInnerWords.join('') + '\n').repeat(quantity));
+	}
+	return resultArr.join('').replace(/\n$/, '');
+};
 
-// сортируем только не четные
-console.log([5, 7, 3, 9, 2, 6].sort((a, b) => a % 2 && a - b)); // [3,5,7,9,2,6]
+console.log(scale('abcd', 2, 3));
+console.log(scale('', 5, 5));
+console.log(scale('Kj\nSH', 1, 2)); // "Kj\nKj\nSH\nSH"
 
-const average = (scores) =>
-	Math.round(scores.reduce((a, b) => a + b) / scores.length);
+let a = 1;
+let b = 2;
+
+[a, b] = [b, a];
+
+console.log(a);
+console.log(b);
+
+function add(a, b) {
+	const result = '';
+	const temp = 0;
+	a = a.split('');
+	b = b.split('');
+	while (a.length || b.length || temp) {
+		temp += ~~a.pop() + ~~b.pop();
+		result = (temp % 10) + result;
+		temp = temp > 9;
+	}
+	return result;
+}
+
+const factorial = (n) => {
+	return n <= 1 ? 1 : n * factorial(n - 1);
+};
--- codewarsTs.ts
+++ codewarsTs.ts
@@ -1,12 +1,69 @@
-function splitList(values: number[]): number[][] {
-	return [
-		values.slice(0, Math.round(values.length / 2)),
-		values.slice(Math.round(values.length / 2)),
-	];
+function absoluteSorting(values: number[]): number[] {
+	return values.sort((a, b) => Math.abs(a) - Math.abs(b));
 }
 
 console.log('Example:');
-console.log(splitList([1, 2, 3, 4, 5, 6])); // [[1, 2, 3], [4, 5, 6]]
-console.log(splitList([1, 2, 3])); // [[1, 2], [3]]
-console.log(splitList([1])); // [[1], []])
-console.log(splitList([])); // [[], []]
+console.log(absoluteSorting([-20, -5, 10, 15])); // [ -5, 10, 15, -20]
+absoluteSorting([1, 2, 3, 0]); // [0, 1, 2, 3]
+
+function goesAfter(word: string, first: string, second: string): boolean {
+	const indexSecond = word.indexOf(second);
+	return !![...word].find(
+		(el, index, arr) =>
+			el === first && arr[index + 1] === second && index + 1 === indexSecond
+	);
+}
+
+console.log('Example:');
+console.log(goesAfter('world', 'w', 'o')); // true
+console.log(goesAfter('world', 'w', 'r')); // false
+console.log(goesAfter('almaz', 'm', 'a')); // false
+
+function timeConverter(dayTime) {
+	const hour = Number(dayTime.slice(0, 2));
+	return hour > 12
+		? `${hour - 12}${dayTime.slice(2)} p.m.`
+		: hour === 12
+		? `${hour}${dayTime.slice(2)} p.m.`
+		: hour === 0
+		? `${hour + 12}${dayTime.slice(2)} a.m.`
+		: `${hour}${dayTime.slice(2)} a.m.`;
+}
+
+console.log('Example:');
+console.log(timeConverter('12:30')); // '0:30 p.m.'
+console.log(timeConverter('09:00')); // '9:00 a.m.'
+console.log(timeConverter('23:15')); // '11:15 p.m.'
+console.log(timeConverter('00:30')); // 12:30 p.m.
+console.log(timeConverter('00:00')); // 12:00 a.m.
+
+function sumByTypes(values: Array<number | string>): [string, number] {
+	return values.reduce(
+		(acc, el) => {
+			if (typeof el === 'number') {
+				acc[1] += el;
+			} else {
+				acc[0] += el;
+			}
+			return acc;
+		},
+		['', 0]
+	);
+}
+console.log(sumByTypes(['size', 12, 'in', 45, 0])); // ['sizein', 57]
+
+function translate(text: string): any {
+	return [...text]
+		.map((el, index, arr) =>
+			/[aeiouy]/.test(el) && !/[aeiouy]/.test(arr[index - 1]) ? '' : el
+		)
+		.join('')
+		.replace(/a+|e+|i+|o+|u+|y+/g, (x) =>
+			x.length <= 3 ? x[0] : x[0].repeat(Math.ceil(x.length / 3))
+		);
+}
+
+console.log(translate('hieeelalaooo')); //'hello'
+console.log(translate('hoooowe yyyooouuu duoooiiine')); //how you doin'
+console.log(translate('aaa bo cy da eee fe')); //'a b c d e f'
+console.log(translate('sooooso aaaaaaaaa')); // 'sos aaa'
