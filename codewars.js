8a0ad6fcfae35c3a0c3ff3ed977d012d89e9553b
Ivan Vidutin <Forumuse@yandex.ru>
2021-12-21 12:28:30 +0500

    21122021

--- codewars.js
+++ codewars.js
@@ -1,2 +1,44 @@
-const discoverOriginalPrice = (discountedPrice, salePercentage) =>
-	((discountedPrice / (100 - salePercentage)) * 100).toFixed(2) * 1;
+import { performance } from 'perf_hooks';
+
+const maxTests = 200;
+const testLoopLimit = 1000000;
+
+function runTest(index = 1, results = { usingVar: 0, usingLet: 0 }) {
+	console.log(`Running Test #${index} of ${maxTests}`);
+	setTimeout(() => {
+		const varTime = usingVar();
+		const letTime = usingLet();
+		results.usingVar += varTime;
+		results.usingLet += letTime;
+		console.log(`Test ${index}: var = ${varTime.toFixed(2)}ms, let = ${letTime.toFixed(2)}ms`);
+		++index;
+		if (index <= maxTests) {
+			setTimeout(() => runTest(index, results), 0);
+		} else {
+			console.log(`Average time with var: ${(results.usingVar / maxTests).toFixed(2)}ms`);
+			console.log(`Average time with let: ${(results.usingLet / maxTests).toFixed(2)}ms`);
+		}
+	}, 0);
+}
+
+function usingVar() {
+	const start = performance.now();
+	let x = 0;
+	for (var i = 0; i < testLoopLimit; i++) {
+		x += i;
+	}
+	const endTime = performance.now();
+	return endTime - start;
+}
+
+function usingLet() {
+	const start = performance.now();
+	let x = 0;
+	for (let i = 0; i < testLoopLimit; i++) {
+		x += i;
+	}
+	const endTime = performance.now();
+	return endTime - start;
+}
+
+runTest();
