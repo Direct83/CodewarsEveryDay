72afbc5150e38ca4e74b7e8a5a02750a70c7bf5d
Ivan Vidutin <Forumuse@yandex.ru>
2021-12-31 12:23:11 +0500

    31122021

--- code/codewars.js
+++ code/codewars.js
@@ -1,1 +1,7 @@
-const greet = (name) => (name ? `hello ${name}!` : null);
+const min = (arr, toReturn) => {
+	const minValue = Math.min(...arr);
+	return toReturn === 'value' ? minValue : arr.indexOf(minValue);
+};
+
+console.log(min([1, 2, 3, 4, 5], 'value'));
+console.log(min([1, 2, 3, 4, 5], 'index'));
--- completed-code/completedTasks.js
+++ completed-code/completedTasks.js
@@ -911,3 +911,11 @@
 // console.log(spam(14));
 
 // const greet = (name) => (name ? `hello ${name}!` : null);
+
+// const min = (arr, toReturn) => {
+// 	const minValue = Math.min(...arr);
+// 	return toReturn === 'value' ? minValue : arr.indexOf(minValue);
+// };
+
+// console.log(min([1, 2, 3, 4, 5], 'value'));
+// console.log(min([1, 2, 3, 4, 5], 'index'));
