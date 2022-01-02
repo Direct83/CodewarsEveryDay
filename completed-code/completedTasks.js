d0591c7c5c06d666c8dc4c6e203c38766c5dab85
Ivan Vidutin <Forumuse@yandex.ru>
2022-01-02 12:22:38 +0500

    02012022

--- completed-code/completedTasks.js
+++ completed-code/completedTasks.js
@@ -923,3 +923,26 @@
 // const vaporcode = (string) => [...string.replace(/\s/g, '').toUpperCase()].join('  ');
 
 // console.log(vaporcode('Lets go to the movies'));
+
+// const deleteNth1 = (arr, maxRepeats) =>
+// 	arr.length
+// 		? arr.reduce(
+// 				(acc, number, index) => {
+// 					acc.found[number] = ++acc.found[number] || 1;
+// 					if (acc.found[number] <= maxRepeats) {
+// 						acc.arr.push(number);
+// 					}
+// 					return index === arr.length - 1 ? acc.arr : acc;
+// 				},
+// 				{ found: {}, arr: [] }
+// 		  )
+// 		: arr;
+
+// const deleteNth2 = (arr, maxRepeats, cache = {}) =>
+// 	arr.filter((number) => {
+// 		cache[number] = (cache[number] || 0) + 1;
+// 		return cache[number] <= maxRepeats;
+// 	});
+
+// const nums = [1, 1, 1, 1, 2, 2, 2, 2];
+// console.log(deleteNth1(nums, 2));
