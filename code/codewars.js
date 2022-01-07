e2c7f6951724227deb6828d4bc7f05dbe33f3a00
Ivan Vidutin <Forumuse@yandex.ru>
2022-01-07 10:45:56 +0500

    07012022

--- code/codewars.js
+++ code/codewars.js
@@ -1,3 +1,21 @@
-const spacify = (str) => [...str].join(' ');
+const well = (inputArray) =>
+	['Fail!', 'Publish!', 'Publish!', 'I smell a series!'][
+		Math.min(3, inputArray.filter((idea) => idea === 'good').length)
+	];
+// function well(inputArray) {
+// 	const goodCounter = inputArray.filter((idea) => idea === 'good').length;
+// 	const isSeries = goodCounter > 2 ? 'I smell a series!' : 'Publish!';
+// 	return goodCounter !== 0 ? isSeries : 'Fail!';
+// }
+console.log(well(['bad', 'bad', 'bad', 'bad']));
 
-console.log(spacify('hello world')); // 'h e l l o   w o r l d'
+const calculate = (str) =>
+	str
+		.split('plus')
+		.join(' ')
+		.split('minus')
+		.join(' -')
+		.split(' ')
+		.reduce((a, i) => a + +i, 0);
+
+console.log(calculate('1plus2plus3minus4')); // 2
