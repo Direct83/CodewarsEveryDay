a8104cb29cb8bb74910442f24d39cbf66a713d44
Ivan Vidutin <Forumuse@yandex.ru>
2022-01-09 10:05:34 +0500

    09012022

--- code/codewars.js
+++ code/codewars.js
@@ -1,5 +1,42 @@
-const switcher = (x) => x.reduce((a, b) => a + ' ?!abcdefghijklmnopqrstuvwxyz'[29 - b], '');
+const num = '8888888811222333455666777777';
 
-console.log(switcher(['24', '12', '23', '22', '4', '26', '9', '8'])); // 'codewars'
+const luckyNumber = (stringOfNumber) =>
+	[...stringOfNumber].reduce(
+		(acc, number, index) => {
+			const isLastIndex = index === stringOfNumber.length - 1;
+			acc.countNumber[number] = ++acc.countNumber[number] || 1;
+			if (isLastIndex) {
+				acc.luckyNumber = Object.entries(acc.countNumber).reduce((acc, [key, value]) => {
+					acc = Number(key) === value ? value : acc;
+					return acc;
+				}, null);
+			}
+			return isLastIndex ? acc.luckyNumber : acc;
+		},
+		{ countNumber: {}, luckyNumber: null }
+	);
 
-console.log(switcher(['25', '7', '8', '4', '14', '23', '8', '25', '23', '29', '16', '16', '4'])); // 'btswmdsbd kkw'
+console.log(luckyNumber(num));
+
+const argQuery = 'user.name.firstname=Ivan&user.name.secondname=Vidutin&user.city=Perm';
+
+const queryObjectify = (query) => {
+	const rootObject = {};
+	query.split('&').forEach((chunk) => {
+		const [keys, value] = chunk.split('=');
+		let indicator = rootObject;
+		keys.split('.').forEach((key, index, arr) => {
+			if (!indicator[key]) {
+				indicator[key] = index === arr.length - 1 ? value : {};
+			}
+			indicator = indicator[key];
+		});
+	});
+	return rootObject;
+};
+
+console.log(queryObjectify(argQuery));
+
+const reverse = (array) => array.reduce((acc, el) => [el].concat(acc), []);
+
+console.log(reverse([1, 2, 3]));
