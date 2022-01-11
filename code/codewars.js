606858964fd7609f957e46304cda0aad6d31630e
Ivan Vidutin <Forumuse@yandex.ru>
2022-01-11 08:22:54 +0500

    11012022

--- code/codewars.js
+++ code/codewars.js
@@ -1,38 +1,12 @@
-const incrementer = (numbers) => numbers.map((number, index) => (++index + number) % 10);
-
-console.log(incrementer([4, 6, 7, 1, 3])); // [ 5, 8, 0, 5, 8 ]
-
-function sum(digits) {
-	try {
-		const numbers = digits
-			.toString()
-			.split('')
-			.map((symbol) => parseInt(symbol));
-		return `${numbers.join(' + ')} = ${numbers.reduce((a, b) => a + b)}`;
-	} catch (e) {
-		return '';
-	}
-}
-
-console.log(sum('268482898706045')); // 2 + 6 + 8 + 4 + 8 + 2 + 8 + 9 + 8 + 7 + 0 + 6 + 0 + 4 + 5 = 77
-
-const betweenExtremes = (numbers) => Math.max(...numbers) - Math.min(...numbers);
-
-console.log(betweenExtremes([21, 34, 54, 43, 26, 12])); // 42
-
-function digitsAverage(input) {
-	if (input < 10) {
-		return input;
-	}
-	while (input > 9) {
-		input = String(input);
-		let array = [];
-		for (let i = 0; i < input.length - 1; i++) {
-			array.push(Math.round((+input[i] + +input[i + 1]) / 2));
+function findAB(numbers, c) {
+	for (let i = 0; i < numbers.length; i++) {
+		for (let j = 1 + i; j < numbers.length; j++) {
+			if (numbers[i] * numbers[j] === c) {
+				return [numbers[i], numbers[j]];
+			}
 		}
-		input = +array.join('');
 	}
-	return input;
+	return null;
 }
 
-console.log(digitsAverage(246)); // 4
+console.log(findAB([1, 2, 3], 3));
--- completed-code/completedTasks.js
+++ completed-code/completedTasks.js
@@ -1124,3 +1124,16 @@
 // }
 
 // console.log(digitsAverage(246)); // 4
+
+// function findAB(numbers, c) {
+// 	for (let i = 0; i < numbers.length; i++) {
+// 		for (let j = 1 + i; j < numbers.length; j++) {
+// 			if (numbers[i] * numbers[j] === c) {
+// 				return [numbers[i], numbers[j]];
+// 			}
+// 		}
+// 	}
+// 	return null;
+// }
+
+// console.log(findAB([1, 2, 3], 3));
