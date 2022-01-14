30bbb1609f331815372745490faec5071a98eed8
Ivan Vidutin <Forumuse@yandex.ru>
2022-01-14 11:15:40 +0500

    14012022

--- code/codewars.js
+++ code/codewars.js
@@ -1,4 +1,8 @@
-const reOrdering = (text) => text.replace(/(.+) ([A-Z]\w*)/, `$2 $1`);
+const hydrate = (s) => {
+	const count = s.match(/\d/g).reduce((acc, el) => acc + Number(el), 0);
+	const isPlural = count !== 1 ? 'es' : '';
+	return `${count} glass${isPlural} of water`;
+};
 
-console.log(reOrdering('ming Yao'));
-console.log(reOrdering('bull color pig Patrick'));
+console.log(hydrate('2 glasses of wine and 1 shot')); // 3 glasses of water
+console.log(hydrate('1 beer')); // 1 glass of water
--- completed-code/completedTasks.js
+++ completed-code/completedTasks.js
@@ -1146,3 +1146,12 @@
 
 // console.log(reOrdering('ming Yao'));
 // console.log(reOrdering('bull color pig Patrick'));
+
+// const hydrate = (s) => {
+// 	const count = s.match(/\d/g).reduce((acc, el) => acc + Number(el), 0);
+// 	const isPlural = count !== 1 ? 'es' : '';
+// 	return `${count} glass${isPlural} of water`;
+// };
+
+// console.log(hydrate('2 glasses of wine and 1 shot')); // 3 glasses of water
+// console.log(hydrate('1 beer')); // 1 glass of water
