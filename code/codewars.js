17564a415024cc18edc7e6b43e3f85ea20cf5047
Ivan Vidutin <Forumuse@yandex.ru>
2022-01-17 10:05:56 +0500

    17012022

--- code/codewars.js
+++ code/codewars.js
@@ -1,6 +1,9 @@
-const pattern = (n) =>
-	n < 1
-		? ''
-		: [...Array(n)].map((_, index) => `${++index}`.repeat(index)).join('\n');
+const findEvenIndex = (arr) =>
+	arr.findIndex(
+		(_, idx) =>
+			arr.slice(0, idx + 1).reduce((pre, val) => pre + val) ===
+			arr.slice(idx).reduce((pre, val) => pre + val)
+	);
 
-console.log(pattern(2));
+console.log(findEvenIndex([1, 2, 3, 4, 3, 2, 1]));
+console.log(findEvenIndex([20, 10, 30, 10, 10, 15, 35]));
--- completed-code/completedTasks.js
+++ completed-code/completedTasks.js
@@ -1166,3 +1166,13 @@
 // 		: [...Array(n)].map((_, index) => `${++index}`.repeat(index)).join('\n');
 
 // console.log(pattern(2));
+
+// const findEvenIndex = (arr) =>
+// 	arr.findIndex(
+// 		(_, idx) =>
+// 			arr.slice(0, idx + 1).reduce((pre, val) => pre + val) ===
+// 			arr.slice(idx).reduce((pre, val) => pre + val)
+// 	);
+
+// console.log(findEvenIndex([1, 2, 3, 4, 3, 2, 1]));
+// console.log(findEvenIndex([20, 10, 30, 10, 10, 15, 35]));
