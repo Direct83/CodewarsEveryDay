3cb232fcdadbc734565ce279c79722cdad0e3adb
Ivan Vidutin <Forumuse@yandex.ru>
2022-01-23 10:22:05 +0500

    23012022

--- code/codewars.js
+++ code/codewars.js
@@ -1,4 +1,11 @@
-const getSumOfDigits = (integer) =>
-	[...String(integer)].reduce((acc, number) => acc + Number(number), 0);
+const firstSlash = '/';
+const rest = '/str/number'.slice(1);
 
-console.log(getSumOfDigits(123)); // 6
+console.log(firstSlash + rest.slice(0, rest.indexOf('/')));
+
+const solve = (str) => {
+	const symbols = [...str].filter((symbol) => symbol !== ' ');
+	return str.replace(/\S/g, (_) => symbols.pop());
+};
+
+console.log(solve('i love codewars')); // s rawe docevoli
