368f11f4e006f25f56ac9e55005f612fcbd70293
Ivan Vidutin <Forumuse@yandex.ru>
2022-01-25 08:58:55 +0500

    25012022

--- code/codewars.js
+++ code/codewars.js
@@ -1,9 +1,1 @@
-const dbSort = (array) =>
-	array.sort(
-		(a, b) =>
-			(typeof a === 'string') - (typeof b === 'string') || (a > b) - (a < b)
-	);
-console.log(
-	dbSort(['Apple', 46, '287', 574, 'Peach', '3', '69', 78, 'Grape', '423'])
-);
-// [46, 78, 574, '287', '3', '423', '69', 'Apple', 'Grape', 'Peach']
+const problem = (x) => (typeof x === 'string' ? 'Error' : x * 50 + 6);
