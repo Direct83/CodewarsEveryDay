d3cb84c5d69ae4923233400a655a38748de92d82
Ivan Vidutin <Forumuse@yandex.ru>
2022-02-04 08:49:42 +0500

    04022022

--- code/codewars.js
+++ code/codewars.js
@@ -1,3 +1,13 @@
-String.prototype.isLetter = function () {
-	return /^[a-zA-Z]$/g.test(this);
-};
+const reverse = (number) =>
+	Math.sign(number) * [...String(Math.abs(number))].reverse().join('');
+
+console.log(reverse(-423)); // -324
+
+const isWalidWalk = (walk) =>
+	walk.length + 1 !== 10 &&
+	walk.reduce((acc, el, index) => {
+		acc[el] = ++acc[el] || 1;
+		return index === walk.length - 1 ? acc.n === acc.s && acc.w === acc.e : acc;
+	}, {});
+
+console.log(isWalidWalk(['n', 's', 'w', 'e', 'n', 's', 'n', 's', 'w', 'e'])); // true
