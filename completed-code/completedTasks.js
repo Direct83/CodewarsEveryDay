25ff4bbe6f78ab5bf9aca7e5513568dbe24c509f
Ivan Vidutin <Forumuse@yandex.ru>
2022-02-10 11:09:14 +0500

    10022022

--- completed-code/completedTasks.js
+++ completed-code/completedTasks.js
@@ -1287,3 +1287,21 @@
 // 		.split(' ')
 // 		.map((word) => word[0])
 // 		.join('');
+
+// const giveMeFive = (obj) =>
+// 	Object.entries(obj).reduce((acc, el) => {
+// 		if (el[0].length === 5) {
+// 			acc = acc.concat(el[0]);
+// 		}
+// 		if (el[1].length === 5) {
+// 			acc = acc.concat(el[1]);
+// 		}
+// 		return acc;
+// 	}, []);
+
+// function palindrome(num) {
+// 	if (typeof num === 'string' || num < 0) {
+// 		return 'Not valid';
+// 	}
+// 	return [...String(num)].reverse().join('') === String(num);
+// }
