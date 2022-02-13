6e0b477a09fe3e37bd124f63dc18192c26c85008
Ivan Vidutin <Forumuse@yandex.ru>
2022-02-13 10:24:54 +0500

    13022022

--- completed-code/completedTasks.js
+++ completed-code/completedTasks.js
@@ -1311,3 +1311,24 @@
 // function args_count() {
 // 	return arguments.length;
 // }
+
+// const solve = (s) =>
+// 	Object.values(
+// 		[...s].reduce((acc, el) => {
+// 			if (/[A-Z]/.test(el)) {
+// 				acc[1] = ++acc[1] || 1;
+// 			}
+// 			if (/[a-z]/.test(el)) {
+// 				acc[2] = ++acc[2] || 1;
+// 			}
+// 			if (/[0-9]/.test(el)) {
+// 				acc[3] = ++acc[3] || 1;
+// 			}
+// 			if (/[^\w]/.test(el)) {
+// 				acc[4] = ++acc[4] || 1;
+// 			}
+// 			return acc;
+// 		}, {})
+// 	);
+
+// console.log(solve('!@#$AAAaaaaa12'));
