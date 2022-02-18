5fe0ce0173c1456434d95f5fe659a99eb968d195
Ivan Vidutin <Forumuse@yandex.ru>
2022-02-18 09:27:49 +0500

    18022022

--- code/codewars.js
+++ code/codewars.js
@@ -1,11 +1,10 @@
-const validateWord = (string) =>
-	[...string.toLowerCase()].reduce((acc, symbol, index, arr) => {
-		acc[symbol] = ++acc[symbol] || 1;
+const checkThreeAndTwo = (array) =>
+	array.reduce((acc, el, index, arr) => {
+		acc[el] = ++acc[el] || 1;
 		if (index === arr.length - 1) {
-			const values = Object.values(acc);
-			return values.every((el) => el === values[0]);
+			return Object.values(acc).every((el) => [3, 2].includes(el));
 		}
 		return acc;
 	}, {});
 
-console.log(validateWord('Abcabc'));
+console.log(checkThreeAndTwo(['a', 'a', 'a', 'b']));
--- completed-code/completedTasks.js
+++ completed-code/completedTasks.js
@@ -1352,3 +1352,14 @@
 // 	}, {});
 
 // console.log(validateWord('Abcabc'));
+
+// const checkThreeAndTwo = (array) =>
+// 	array.reduce((acc, el, index, arr) => {
+// 		acc[el] = ++acc[el] || 1;
+// 		if (index === arr.length - 1) {
+// 			return Object.values(acc).every((el) => [3, 2].includes(el));
+// 		}
+// 		return acc;
+// 	}, {});
+
+// console.log(checkThreeAndTwo(['a', 'a', 'a', 'b']));
