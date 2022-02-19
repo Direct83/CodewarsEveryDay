837147ef7a3860ab97e9b7e273d9e5498b35cb5c
Ivan Vidutin <Forumuse@yandex.ru>
2022-02-19 11:10:07 +0500

    19022022

--- code/codewars.js
+++ code/codewars.js
@@ -1,10 +1,4 @@
-const checkThreeAndTwo = (array) =>
-	array.reduce((acc, el, index, arr) => {
-		acc[el] = ++acc[el] || 1;
-		if (index === arr.length - 1) {
-			return Object.values(acc).every((el) => [3, 2].includes(el));
-		}
-		return acc;
-	}, {});
+const uniqueSum = (lst) =>
+	[...new Set(lst)].reduce((acc, number) => acc + number, null);
 
-console.log(checkThreeAndTwo(['a', 'a', 'a', 'b']));
+console.log(uniqueSum([1, 3, 8, 1, 8])); // 12
--- completed-code/completedTasks.js
+++ completed-code/completedTasks.js
@@ -1363,3 +1363,8 @@
 // 	}, {});
 
 // console.log(checkThreeAndTwo(['a', 'a', 'a', 'b']));
+
+// const uniqueSum = (lst) =>
+// 	[...new Set(lst)].reduce((acc, number) => acc + number, null);
+
+// console.log(uniqueSum([1, 3, 8, 1, 8])); // 12
