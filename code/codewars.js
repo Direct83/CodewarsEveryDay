f45c75b0f72eb66d6feacc59c0cb63f0e1a00158
Ivan Vidutin <Forumuse@yandex.ru>
2022-03-11 12:14:12 +0500

    11032022

--- code/codewars.js
+++ code/codewars.js
@@ -1,4 +1,6 @@
-const validateCode = (code) => /^[1-3]/.test(code);
+const mergeArrays = (firstArray, secondArray) =>
+	[...Array(Math.max(firstArray.length, secondArray.length))]
+		.reduce((acc, _, i) => acc.concat(firstArray[i], secondArray[i]), [])
+		.filter(Boolean);
 
-console.log(validateCode(423));
-console.log(validateCode(323));
+console.log(mergeArrays([1, 2, 3, 4, 5, 6, 7, 8], ['a', 'b', 'c']));
--- completed-code/completedTasks.js
+++ completed-code/completedTasks.js
@@ -1478,3 +1478,10 @@
 
 // console.log(validateCode(423));
 // console.log(validateCode(323));
+
+// const mergeArrays = (firstArray, secondArray) =>
+// 	[...Array(Math.max(firstArray.length, secondArray.length))]
+// 		.reduce((acc, _, i) => acc.concat(firstArray[i], secondArray[i]), [])
+// 		.filter(Boolean);
+
+// console.log(mergeArrays([1, 2, 3, 4, 5, 6, 7, 8], ['a', 'b', 'c']));
