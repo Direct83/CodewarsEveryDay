c2ba9633760d7a0b747a726f609483c7a5e8925f
Ivan Vidutin <Forumuse@yandex.ru>
2022-03-24 12:16:35 +0500

    24032022

--- code/codewars.js
+++ code/codewars.js
@@ -1,6 +1,10 @@
-function getFirstPython(list) {
-	const phytonUser = list.find((user) => user.language === 'Python');
-	return phytonUser
-		? `${phytonUser.firstName}, ${phytonUser.country}`
-		: 'There will be no Python developers';
-}
+const jumpingNumber = (number) =>
+	[...String(number)].every(
+		(number, index, array) =>
+			index < 1 || Math.abs(number - array[index - 1]) === 1
+	)
+		? 'Jumping!!'
+		: 'Not!!';
+
+console.log(jumpingNumber(79));
+console.log(jumpingNumber(98789876));
--- completed-code/completedTasks.js
+++ completed-code/completedTasks.js
@@ -1564,3 +1564,14 @@
 // 		? `${phytonUser.firstName}, ${phytonUser.country}`
 // 		: 'There will be no Python developers';
 // }
+
+// const jumpingNumber = (number) =>
+// 	[...String(number)].every(
+// 		(number, index, array) =>
+// 			index < 1 || Math.abs(number - array[index - 1]) === 1
+// 	)
+// 		? 'Jumping!!'
+// 		: 'Not!!';
+
+// console.log(jumpingNumber(79));
+// console.log(jumpingNumber(98789876));
