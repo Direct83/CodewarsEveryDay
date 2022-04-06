5c4a6fdabbe05e997ebae42a974046325f39de26
Ivan Vidutin <Forumuse@yandex.ru>
2022-04-06 09:22:48 +0500

    06042022

--- code/codewars.js
+++ code/codewars.js
@@ -1,3 +1,6 @@
-const catMouse = (x) => (x.length <= 5 ? 'Caught!' : 'Escaped!');
+const unluckyDays = (year) =>
+	[...Array(12)].filter((_, index) => new Date(year, index, 13).getDay() === 5)
+		.length;
 
-console.log(catMouse('C....m'));
+console.log(unluckyDays(1586));
+console.log(unluckyDays(1001));
--- completed-code/completedTasks.js
+++ completed-code/completedTasks.js
@@ -1618,3 +1618,10 @@
 // const catMouse = (x) => (x.length <= 5 ? 'Caught!' : 'Escaped!');
 
 // console.log(catMouse('C....m'));
+
+// const unluckyDays = (year) =>
+// 	[...Array(12)].filter((_, index) => new Date(year, index, 13).getDay() === 5)
+// 		.length;
+
+// console.log(unluckyDays(1586));
+// console.log(unluckyDays(1001));
