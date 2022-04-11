2b8d6f48ddda78f97038d2b6c0c6cb117e9eb915
Ivan Vidutin <Forumuse@yandex.ru>
2022-04-11 09:03:56 +0500

    11042022

--- completed-code/completedTasks.js
+++ completed-code/completedTasks.js
@@ -1633,3 +1633,46 @@
 // console.log(specialNumber(2783));
 
 // const removeVowels = (str) => str.replace(/[aeiou]/g, '');
+
+// const list1 = [
+// 	{
+// 		firstName: 'Noah',
+// 		lastName: 'M.',
+// 		country: 'Switzerland',
+// 		continent: 'Europe',
+// 		age: 19,
+// 		language: 'C',
+// 	},
+// 	{
+// 		firstName: 'Anna',
+// 		lastName: 'R.',
+// 		country: 'Liechtenstein',
+// 		continent: 'Europe',
+// 		age: 52,
+// 		language: 'JavaScript',
+// 	},
+// 	{
+// 		firstName: 'Ramon',
+// 		lastName: 'R.',
+// 		country: 'Paraguay',
+// 		continent: 'Americas',
+// 		age: 29,
+// 		language: 'Ruby',
+// 	},
+// 	{
+// 		firstName: 'George',
+// 		lastName: 'B.',
+// 		country: 'England',
+// 		continent: 'Europe',
+// 		age: 81,
+// 		language: 'C',
+// 	},
+// ];
+
+// const countLanguages = (list) =>
+// 	list.reduce((acc, people) => {
+// 		acc[people.language] = ++acc[people.language] || 1;
+// 		return acc;
+// 	}, {});
+
+// console.log(countLanguages(list1));
