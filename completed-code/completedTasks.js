27bc7a7fa6254dac80a0147eb79e712011cb68e8
Ivan Vidutin <Forumuse@yandex.ru>
2022-04-18 09:22:58 +0500

    18042022

--- completed-code/completedTasks.js
+++ completed-code/completedTasks.js
@@ -1715,3 +1715,64 @@
 // 	friday.concat(saturday).reduce((acc, el) => acc - el, total);
 
 // console.log(lostSheep([1, 2], [3, 4], 15));
+
+// const list1 = [
+// 	{
+// 		firstName: 'Daniel',
+// 		lastName: 'J.',
+// 		country: 'Aruba',
+// 		continent: 'Americas',
+// 		age: 42,
+// 		language: 'JavaScript',
+// 	},
+// 	{
+// 		firstName: 'Kseniya',
+// 		lastName: 'T.',
+// 		country: 'Belarus',
+// 		continent: 'Europe',
+// 		age: 22,
+// 		language: 'JavaScript',
+// 	},
+// 	{
+// 		firstName: 'Hanna',
+// 		lastName: 'L.',
+// 		country: 'Hungary',
+// 		continent: 'Europe',
+// 		age: 65,
+// 		language: 'JavaScript',
+// 	},
+// ];
+
+// const list2 = [
+// 	{
+// 		firstName: 'Mariami',
+// 		lastName: 'G.',
+// 		country: 'Georgia',
+// 		continent: 'Europe',
+// 		age: 29,
+// 		language: 'Python',
+// 	},
+// 	{
+// 		firstName: 'Mia',
+// 		lastName: 'H.',
+// 		country: 'Germany',
+// 		continent: 'Europe',
+// 		age: 39,
+// 		language: 'Ruby',
+// 	},
+// 	{
+// 		firstName: 'Maria',
+// 		lastName: 'I.',
+// 		country: 'Greece',
+// 		continent: 'Europe',
+// 		age: 32,
+// 		language: 'C',
+// 	},
+// ];
+
+// function isSameLanguage(list) {
+// 	const language = list[0].language;
+// 	return list.every((employee) => language === employee.language);
+// }
+
+// console.log(isSameLanguage(list2));
