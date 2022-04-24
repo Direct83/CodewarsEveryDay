ed41dcdece5174bb8b4ed84928f04015784aeff8
Ivan Vidutin <Forumuse@yandex.ru>
2022-04-24 12:03:22 +0500

    24042022

--- completed-code/completedTasks.js
+++ completed-code/completedTasks.js
@@ -1824,3 +1824,22 @@
 
 // console.log(modifiedSum([1, 2, 3], 3));
 // console.log(modifiedSum([1, 2], 5));
+
+// const minimumSteps = (numbers, value) =>
+// 	numbers
+// 		.sort((a, b) => a - b)
+// 		.reduce(
+// 			(acc, el, index) => {
+// 				if (acc.sum < value) {
+// 					acc.sum += el;
+// 					acc.count = index;
+// 				}
+// 				return acc;
+// 			},
+// 			{ sum: 0, count: 0 }
+// 		).count;
+
+// console.log(minimumSteps([19, 98, 69, 28, 75, 45, 17, 98, 67], 464));
+// console.log(minimumSteps([8, 9, 10, 4, 2], 23));
+// console.log(minimumSteps([10, 9, 9, 8], 17));
+// console.log(minimumSteps([4, 6, 3], 7));
