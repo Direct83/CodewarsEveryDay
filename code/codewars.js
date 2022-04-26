6e5260c3f907671a350596c6e3bd20b48cf62a8c
Ivan Vidutin <Forumuse@yandex.ru>
2022-04-26 09:47:19 +0500

    26042022

--- code/codewars.js
+++ code/codewars.js
@@ -1,7 +1,13 @@
-function meeting(x) {
-	const index = x.findIndex((el) => el === 'O');
-	return index > -1 ? index : 'None available!';
-}
+const maxGap = (numbers) =>
+	numbers
+		.sort((a, b) => a - b)
+		.reduce((acc, el, index, arr) => {
+			const number = Math.abs(el - arr[index + 1]);
+			if (number > acc) {
+				acc = number;
+			}
+			return acc;
+		}, 0);
 
-console.log(meeting(['X', 'O', 'X']));
-console.log(meeting(['X', 'X', 'X', 'X', 'X']));
+console.log(maxGap([13, 10, 2, 9, 5]));
+console.log(maxGap([24, 299, 131, 14, 26, 25]));
--- completed-code/completedTasks.js
+++ completed-code/completedTasks.js
@@ -1851,3 +1851,17 @@
 
 // console.log(meeting(['X', 'O', 'X']));
 // console.log(meeting(['X', 'X', 'X', 'X', 'X']));
+
+// const maxGap = (numbers) =>
+// 	numbers
+// 		.sort((a, b) => a - b)
+// 		.reduce((acc, el, index, arr) => {
+// 			const number = Math.abs(el - arr[index + 1]);
+// 			if (number > acc) {
+// 				acc = number;
+// 			}
+// 			return acc;
+// 		}, 0);
+
+// console.log(maxGap([13, 10, 2, 9, 5]));
+// console.log(maxGap([24, 299, 131, 14, 26, 25]));
