588404e128bd150692351f7ed2abe5f92e64bb81
Ivan Vidutin <Forumuse@yandex.ru>
2022-04-30 11:05:57 +0500

    30042022

--- code/codewars.js
+++ code/codewars.js
@@ -1,5 +1,12 @@
-const lastSurvivor = (letters, coords) =>
-	coords.reduce(
-		(acc, coord) => acc.slice(0, coord) + acc.slice(coord + 1),
-		letters
-	);
+const countChar = (string, char) =>
+	[...string].reduce((acc, el) => {
+		if (el.toLowerCase() === char.toLowerCase()) {
+			++acc;
+		}
+		return acc;
+	}, 0);
+
+const countChar2 = (str, count) => str.match(new RegExp(count, 'gi')).length;
+
+console.log(countChar('Fancy fifth fly aloof', 'f'));
+console.log(countChar2('Fancy fifth fly aloof', 'f'));
