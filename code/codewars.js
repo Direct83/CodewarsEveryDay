15926aa8294031f6125257d1d14566a04389b2aa
Ivan Vidutin <Forumuse@yandex.ru>
2022-05-09 10:19:25 +0500

    09052022

--- code/codewars.js
+++ code/codewars.js
@@ -1,42 +1,28 @@
-const crap = (x, bags, cap) =>
-	x.flat(1).includes('D')
-		? 'Dog!!'
-		: bags * cap - x.flat(1).filter((el) => el === '@').length > 0
-		? 'Clean'
-		: 'Cr@p';
+const findMissingNumber = (sequence) => {
+	if (!sequence.length) {
+		return 0;
+	}
 
-console.log(
-	crap(
-		[
-			['_', '_', '_', '_'],
-			['_', '_', '_', '@'],
-			['_', '_', '@', '_'],
-		],
-		2,
-		2
-	)
-);
+	const arr = sequence
+		.split(' ')
+		.sort((a, b) => a - b)
+		.map(Number);
 
-console.log(
-	crap(
-		[
-			['_', '_', '_', '_'],
-			['_', '_', '_', '@'],
-			['_', '_', '@', '_'],
-		],
-		1,
-		1
-	)
-);
+	if (!arr.every((el) => !isNaN(el))) {
+		return 1;
+	}
 
-console.log(
-	crap(
-		[
-			['_', '_'],
-			['_', '@'],
-			['D', '_'],
-		],
-		2,
-		2
-	)
-);
+	return arr.reduce((acc, el, index) => {
+		if (el != index + 1 && !acc) {
+			return (acc += index + 1);
+		}
+		return acc;
+	}, 0);
+};
+
+console.log(findMissingNumber('2 6 4 5 3'));
+console.log(findMissingNumber('1 2 3 5'));
+console.log(findMissingNumber('2 6 1 5 3'));
+console.log(findMissingNumber('1 3'));
+console.log(findMissingNumber('1 5'));
+console.log(findMissingNumber('2 1 4 3 a'));
--- completed-code/completedTasks.js
+++ completed-code/completedTasks.js
@@ -2199,3 +2199,32 @@
 // 		2
 // 	)
 // );
+
+// const findMissingNumber = (sequence) => {
+// 	if (!sequence.length) {
+// 		return 0;
+// 	}
+
+// 	const arr = sequence
+// 		.split(' ')
+// 		.sort((a, b) => a - b)
+// 		.map(Number);
+
+// 	if (!arr.every((el) => !isNaN(el))) {
+// 		return 1;
+// 	}
+
+// 	return arr.reduce((acc, el, index) => {
+// 		if (el != index + 1 && !acc) {
+// 			return (acc += index + 1);
+// 		}
+// 		return acc;
+// 	}, 0);
+// };
+
+// console.log(findMissingNumber('2 6 4 5 3'));
+// console.log(findMissingNumber('1 2 3 5'));
+// console.log(findMissingNumber('2 6 1 5 3'));
+// console.log(findMissingNumber('1 3'));
+// console.log(findMissingNumber('1 5'));
+// console.log(findMissingNumber('2 1 4 3 a'));
