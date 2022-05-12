9a31fa82564eb41bb2d17caf88d61ef6d3c52f5c
Ivan Vidutin <Forumuse@yandex.ru>
2022-05-12 11:26:04 +0500

    12052022

--- code/codewars.js
+++ code/codewars.js
@@ -1,4 +1,9 @@
-const productArray = (numbers) =>
-	numbers.map((el) => numbers.reduce((a, b) => a * b) / el);
+function newAvg(arr, newavg) {
+	const navg = newavg * (arr.length + 1) - arr.reduce((acc, el) => acc + el, 0);
+	if (navg <= 0) {
+		throw 'ValueError';
+	}
+	return Math.ceil(navg);
+}
 
-console.log(productArray([16, 17, 4, 3, 5, 2]));
+console.log(newAvg([14, 30, 5, 7, 9, 11, 16], 90));
--- completed-code/completedTasks.js
+++ completed-code/completedTasks.js
@@ -2259,3 +2259,13 @@
 // 	numbers.map((el) => numbers.reduce((a, b) => a * b) / el);
 
 // console.log(productArray([16, 17, 4, 3, 5, 2]));
+
+// function newAvg(arr, newavg) {
+// 	const navg = newavg * (arr.length + 1) - arr.reduce((acc, el) => acc + el, 0);
+// 	if (navg <= 0) {
+// 		throw 'ValueError';
+// 	}
+// 	return Math.ceil(navg);
+// }
+
+// console.log(newAvg([14, 30, 5, 7, 9, 11, 16], 90));
