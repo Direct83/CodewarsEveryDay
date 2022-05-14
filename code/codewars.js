cd932a2457991fd262f0c3d8d1a75b5c49e7907c
Ivan Vidutin <Forumuse@yandex.ru>
2022-05-14 11:22:25 +0500

    14052022

--- code/codewars.js
+++ code/codewars.js
@@ -1,1 +1,14 @@
-const circleOfNumbers = (n, firstNumber) => (firstNumber + n / 2) % n;
+const getStrings = (city) =>
+	[...city.toLowerCase()].reduce((acc, el, index, arr) => {
+		if (/[a-z]/i.test(el)) {
+			acc[el] = ++acc[el] || 1;
+		}
+		return arr.length - 1 === index
+			? Object.entries(acc)
+					.map((el) => [el[0], '*'.repeat(el[1])].join(':'))
+					.join(',')
+			: acc;
+	}, {});
+
+console.log(getStrings('Chicagoc'));
+console.log(getStrings('Las Vegas'));
--- completed-code/completedTasks.js
+++ completed-code/completedTasks.js
@@ -2271,3 +2271,18 @@
 // console.log(newAvg([14, 30, 5, 7, 9, 11, 16], 90));
 
 // const circleOfNumbers = (n, firstNumber) =>(firstNumber + n/2) % n
+
+// const getStrings = (city) =>
+// 	[...city.toLowerCase()].reduce((acc, el, index, arr) => {
+// 		if (/[a-z]/i.test(el)) {
+// 			acc[el] = ++acc[el] || 1;
+// 		}
+// 		return arr.length - 1 === index
+// 			? Object.entries(acc)
+// 					.map((el) => [el[0], '*'.repeat(el[1])].join(':'))
+// 					.join(',')
+// 			: acc;
+// 	}, {});
+
+// console.log(getStrings('Chicagoc'));
+// console.log(getStrings('Las Vegas'));
