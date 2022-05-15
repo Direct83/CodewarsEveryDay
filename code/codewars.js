7b60ff584c1be7c172bea5a46bd5eb23cd0febb8
Ivan Vidutin <Forumuse@yandex.ru>
2022-05-15 11:35:58 +0500

    15052022

--- code/codewars.js
+++ code/codewars.js
@@ -1,14 +1,19 @@
-const getStrings = (city) =>
-	[...city.toLowerCase()].reduce((acc, el, index, arr) => {
-		if (/[a-z]/i.test(el)) {
-			acc[el] = ++acc[el] || 1;
-		}
-		return arr.length - 1 === index
-			? Object.entries(acc)
-					.map((el) => [el[0], '*'.repeat(el[1])].join(':'))
-					.join(',')
-			: acc;
-	}, {});
+const save = (sizes, hd) => {
+	if (!sizes.length) {
+		return 0;
+	}
 
-console.log(getStrings('Chicagoc'));
-console.log(getStrings('Las Vegas'));
+	return sizes.reduce(
+		(acc, el, index) => {
+			if ((acc.sum += el) <= hd) {
+				acc.count += 1;
+			}
+			return sizes.length - 1 === index ? acc.count : acc;
+		},
+		{ sum: 0, count: 0 }
+	);
+};
+
+console.log(save([4, 4, 4, 1, 1], 11)); // 2
+console.log(save([4, 8, 15, 16, 23, 42], 108)); // 6
+console.log([], 69);
--- completed-code/completedTasks.js
+++ completed-code/completedTasks.js
@@ -2286,3 +2286,23 @@
 
 // console.log(getStrings('Chicagoc'));
 // console.log(getStrings('Las Vegas'));
+
+// const save = (sizes, hd) => {
+// 	if (!sizes.length) {
+// 		return 0;
+// 	}
+
+// 	return sizes.reduce(
+// 		(acc, el, index) => {
+// 			if ((acc.sum += el) <= hd) {
+// 				acc.count += 1;
+// 			}
+// 			return sizes.length - 1 === index ? acc.count : acc;
+// 		},
+// 		{ sum: 0, count: 0 }
+// 	);
+// };
+
+// console.log(save([4, 4, 4, 1, 1], 11)); // 2
+// console.log(save([4, 8, 15, 16, 23, 42], 108)); // 6
+// console.log([], 69);
