ddfe39bd87704fb84f63527eeaf063d2f0ace82e
Ivan Vidutin <Forumuse@yandex.ru>
2022-05-22 14:19:11 +0500

    22052022

--- code/codewars.js
+++ code/codewars.js
@@ -1,5 +1,15 @@
-const twoOldestAges = (ages) =>
-	ages
-		.sort((a, b) => b - a)
-		.slice(0, 2)
-		.reverse();
+const tidyNumber = (num) =>
+	[...String(num)]
+		.map(Number)
+		.every((el, index, arr) =>
+			el <= arr[index + 1] ? true : arr[index + 1] === undefined ? true : false
+		);
+
+function stringMerge(string1, string2, letter) {
+	const wordOne = string1.slice(0, string1.indexOf(letter));
+	const wordTwo = string2.slice(string2.indexOf(letter));
+	return wordOne + wordTwo;
+}
+
+console.log(stringMerge('abcdefxxxyzz', 'abcxxxyyyxyzz', 'x'));
+console.log(stringMerge('apowiejfoiajsf', 'iwahfeijouh', 'j'));
