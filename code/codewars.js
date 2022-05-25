c2f7295765534acc8fb7fb5c2080575216e80bed
Ivan Vidutin <Forumuse@yandex.ru>
2022-05-25 10:16:59 +0500

    25052022

--- code/codewars.js
+++ code/codewars.js
@@ -1,7 +1,8 @@
-const findAll = (array, n) =>
-	array.reduce((acc, el, index) => {
-		if (el === n) {
-			acc = acc.concat(index);
-		}
-		return acc;
-	}, []);
+const consecutive = (arr, a, b) =>
+	arr.some(
+		(el, index, arr) =>
+			(el === a && arr[index + 1] === b) || (el === b && arr[index + 1] === a)
+	);
+
+console.log(consecutive([1, 3, 5, 7], 3, 7)); // false
+console.log(consecutive([1, 3, 5, 7], 3, 1)); // true
--- completed-code/completedTasks.js
+++ completed-code/completedTasks.js
@@ -2407,3 +2407,12 @@
 // 		}
 // 		return acc;
 // 	}, []);
+
+// const consecutive = (arr, a, b) =>
+// 	arr.some(
+// 		(el, index, arr) =>
+// 			(el === a && arr[index + 1] === b) || (el === b && arr[index + 1] === a)
+// 	);
+
+// console.log(consecutive([1, 3, 5, 7], 3, 7)); // false
+// console.log(consecutive([1, 3, 5, 7], 3, 1)); // true
