b0289e018aac5c28bf194a5b9ecf2c480a4808d8
Ivan Vidutin <Forumuse@yandex.ru>
2022-05-29 13:07:45 +0500

    29052022

--- completed-code/completedTasks.js
+++ completed-code/completedTasks.js
@@ -2423,3 +2423,16 @@
 
 // l = new Array();
 // console.log(l.remove_([1, 1, 2, 3, 1, 2, 3, 4], [1, 3]));
+
+// const duplicates = (array) =>
+// 	[...array].sort().reduce((acc, el, index, array) => {
+// 		if (el === array[index + 1]) {
+// 			++acc;
+// 			array[index + 1] = null;
+// 		}
+// 		return acc;
+// 	}, 0);
+
+// console.log(duplicates([1, 2, 2, 20, 6, 20, 2, 6, 2]));
+// console.log(duplicates([0, 0, 0, 0, 0, 0, 0]));
+// console.log(duplicates([1000, 1000]));
