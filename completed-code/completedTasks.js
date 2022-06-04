bb3c6a5c13b03e968799a3b22ed6087ce39eea1b
Ivan Vidutin <Forumuse@yandex.ru>
2022-06-04 08:36:37 +0500

    04062022-2

--- completed-code/completedTasks.js
+++ completed-code/completedTasks.js
@@ -2468,3 +2468,18 @@
 // 	);
 
 // console.log(evenLast([2, 3, 4, 5]));
+
+// const twoSum = (numbers, target) =>
+// 	numbers
+// 		.map((value, index, arr) => [index, arr.indexOf(target - value, index + 1)])
+// 		.filter((item) => item[1] > -1)[0];
+
+// console.log(twoSum([3, 2, 3], 6));
+// console.log(twoSum([3, 2, 4], 6));
+
+// const addTwoNumbers = (l1, l2) =>
+// 	[
+// 		...String(Number(l1.reverse().join('')) + Number(l2.reverse().join(''))),
+// 	].reverse();
+
+// console.log(addTwoNumbers([2, 4, 3], [5, 6, 4])); // [7,0,8]
