257c60e6fd55e71c4293a8e30d5500aa678e1124
Ivan Vidutin <Forumuse@yandex.ru>
2022-06-07 09:49:32 +0500

    07062022

--- code/codewars.js
+++ code/codewars.js
@@ -1,7 +1,2 @@
-const isNice = (arr) =>
-	arr.length !== 0 &&
-	arr.every((el) => arr.includes(el + 1) || arr.includes(el - 1));
-
-console.log(isNice([2, 10, 9, 3]));
-console.log(isNice([3, 4, 5, 7]));
-console.log(isNice([-2, 5, 0, 5, 12]));
+const longest = (words) =>
+	words.reduce((a, word) => Math.max(a, word.length), 0);
