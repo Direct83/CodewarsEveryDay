6e626b0e179b401e50d1b21cd271d3ee65ee2de0
Ivan Vidutin <Forumuse@yandex.ru>
2022-06-08 09:56:54 +0500

    08062022

--- completed-code/completedTasks.js
+++ completed-code/completedTasks.js
@@ -2494,3 +2494,16 @@
 
 // const longest = (words) =>
 // 	words.reduce((a, word) => Math.max(a, word.length), 0);
+
+// function nextHappyYear(year) {
+// 	let result = 0;
+// 	for (let i = year + 1; ; i++) {
+// 		if ([...new Set(('' + i).split(''))].length === 4) {
+// 			result = i;
+// 			break;
+// 		}
+// 	}
+// 	return result;
+// }
+
+// console.log(nextHappyYear(1001));
