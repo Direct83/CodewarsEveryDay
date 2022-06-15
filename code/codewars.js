b721814e1b8f06911e3dfd66df3529cc0a096a0f
Ivan Vidutin <Forumuse@yandex.ru>
2022-06-15 09:14:22 +0500

    15062022

--- code/codewars.js
+++ code/codewars.js
@@ -1,10 +1,43 @@
-const covfefe = (str) =>
-	str.includes('coverage')
-		? str.replace(/(coverage)/g, 'covfefe')
-		: str + ' covfefe';
+const isNonDecreasingOrder = (number) =>
+	String(number)
+		.split('')
+		.every((el, index, arr) =>
+			arr.length - 1 === index ? true : el <= arr[index + 1]
+		);
 
-console.log(
-	covfefe(
-		'1xhd 1gap 1yv8coveragefxgj e9hh9coverage6w032 klobcoverage68mlf rtss'
-	)
-);
+function sumDigits(number) {
+	let result = 0;
+	let digit;
+
+	while (number) {
+		digit = number % 10;
+		result += digit;
+		number = (number - digit) / 10;
+	}
+
+	return result;
+}
+
+const findAll = (n, k) => {
+	const maxNumber = Number('9'.repeat(k)) + 1;
+	let results = [];
+	let count = Number('1' + '0'.repeat(k - 1));
+
+	while (count < maxNumber) {
+		if (sumDigits(count) === n && isNonDecreasingOrder(count)) {
+			results.push(count);
+		}
+		++count;
+	}
+
+	if (results.length === 0) {
+		return [0];
+	}
+
+	return [results.length, results[0], results[results.length - 1]];
+};
+
+console.log(findAll(10, 3)); // [8, 118, 334]
+console.log(findAll(27, 3)); // [1, 999, 999]
+console.log(findAll(28, 3)); // [0]
+console.log(findAll(10, 8));
--- completed-code/completedTasks.js
+++ completed-code/completedTasks.js
@@ -2562,3 +2562,47 @@
 // 		'1xhd 1gap 1yv8coveragefxgj e9hh9coverage6w032 klobcoverage68mlf rtss'
 // 	)
 // );
+
+// const isNonDecreasingOrder = (number) =>
+// 	String(number)
+// 		.split('')
+// 		.every((el, index, arr) =>
+// 			arr.length - 1 === index ? true : el <= arr[index + 1]
+// 		);
+
+// function sumDigits(number) {
+// 	let result = 0;
+// 	let digit;
+
+// 	while (number) {
+// 		digit = number % 10;
+// 		result += digit;
+// 		number = (number - digit) / 10;
+// 	}
+
+// 	return result;
+// }
+
+// const findAll = (n, k) => {
+// 	const maxNumber = Number('9'.repeat(k)) + 1;
+// 	let results = [];
+// 	let count = Number('1' + '0'.repeat(k - 1));
+
+// 	while (count < maxNumber) {
+// 		if (sumDigits(count) === n && isNonDecreasingOrder(count)) {
+// 			results.push(count);
+// 		}
+// 		++count;
+// 	}
+
+// 	if (results.length === 0) {
+// 		return [0];
+// 	}
+
+// 	return [results.length, results[0], results[results.length - 1]];
+// };
+
+// console.log(findAll(10, 3)); // [8, 118, 334]
+// console.log(findAll(27, 3)); // [1, 999, 999]
+// console.log(findAll(28, 3)); // [0]
+// console.log(findAll(10, 8));
