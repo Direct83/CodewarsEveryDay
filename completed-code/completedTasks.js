1ff5976e91cff4d473cbf9560d9c3d2bc5d5f53a
Ivan Vidutin <Forumuse@yandex.ru>
2022-06-18 10:23:05 +0500

    18062022

--- completed-code/completedTasks.js
+++ completed-code/completedTasks.js
@@ -2664,3 +2664,18 @@
 // console.log(missingNo(arr1), 26);
 // console.log(missingNo(arr2), 40);
 // console.log(missingNo(arr3), 80);
+
+// // Арифметическая прогрессия
+// const arithmeticSequenceElements = (start, difference, length) =>
+// 	[...Array(length)].map((_, index) => start + difference * index).join(', ');
+
+// console.log(arithmeticSequenceElements(1, 2, 5), '1, 3, 5, 7, 9');
+// console.log(arithmeticSequenceElements(1, 0, 5), '1, 1, 1, 1, 1');
+// console.log(
+// 	arithmeticSequenceElements(1, -3, 10),
+// 	'1, -2, -5, -8, -11, -14, -17, -20, -23, -26'
+// );
+// console.log(
+// 	arithmeticSequenceElements(100, -10, 10),
+// 	'100, 90, 80, 70, 60, 50, 40, 30, 20, 10'
+// );
