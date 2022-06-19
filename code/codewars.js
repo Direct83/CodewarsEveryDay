1ac8e63cac2bc8d2c8c8a10b9db9aadfec18cba7
Ivan Vidutin <Forumuse@yandex.ru>
2022-06-19 07:45:23 +0500

    19062022

--- code/codewars.js
+++ code/codewars.js
@@ -1,14 +1,9 @@
-// Арифметическая прогрессия
-const arithmeticSequenceElements = (start, difference, length) =>
-	[...Array(length)].map((_, index) => start + difference * index).join(', ');
+const head = (arr) => arr[0];
+const tail = (arr) => arr.slice(1);
+const init = (arr) => arr.slice(0, -1);
+const last = (arr) => arr[arr.length - 1];
 
-console.log(arithmeticSequenceElements(1, 2, 5), '1, 3, 5, 7, 9');
-console.log(arithmeticSequenceElements(1, 0, 5), '1, 1, 1, 1, 1');
-console.log(
-	arithmeticSequenceElements(1, -3, 10),
-	'1, -2, -5, -8, -11, -14, -17, -20, -23, -26'
-);
-console.log(
-	arithmeticSequenceElements(100, -10, 10),
-	'100, 90, 80, 70, 60, 50, 40, 30, 20, 10'
-);
+console.log(head([5, 1]), 5);
+console.log(tail([1]), []);
+console.log(init([1, 5, 7, 9]), [1, 5, 7]);
+console.log(last([7, 2]), 2);
--- completed-code/completedTasks.js
+++ completed-code/completedTasks.js
@@ -2679,3 +2679,13 @@
 // 	arithmeticSequenceElements(100, -10, 10),
 // 	'100, 90, 80, 70, 60, 50, 40, 30, 20, 10'
 // );
+
+// const head = (arr) => arr[0];
+// const tail = (arr) => arr.slice(1);
+// const init = (arr) => arr.slice(0, -1);
+// const last = (arr) => arr[arr.length - 1];
+
+// console.log(head([5, 1]), 5);
+// console.log(tail([1]), []);
+// console.log(init([1, 5, 7, 9]), [1, 5, 7]);
+// console.log(last([7, 2]), 2);
