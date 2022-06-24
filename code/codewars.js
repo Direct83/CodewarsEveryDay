5c0a12600bb06d37dbe1222c16fec7448b60320c
Ivan Vidutin <Forumuse@yandex.ru>
2022-06-24 09:39:11 +0500

    24062022

--- code/codewars.js
+++ code/codewars.js
@@ -1,17 +1,16 @@
-const one = (arr, fun) => arr.filter(fun).length === 1;
+const arr = [2, 3, 1, 4, 5, 6, 7];
+function maxMinChange(arr) {
+	const max = Math.max(...arr);
+	const min = Math.min(...arr);
+	return arr.map((el, index, arr) => {
+		if (arr.lastIndexOf(max) === index) {
+			el = min;
+		}
+		if (arr.lastIndexOf(min) === index) {
+			el = max;
+		}
+		return el;
+	});
+}
 
-console.log(
-	one([1, 2, 3, 4, 5], function (item) {
-		return item < 2;
-	})
-);
-console.log(
-	one([1, 2, 3, 4, 5], function (item) {
-		return item % 2;
-	})
-);
-console.log(
-	one([1, 2, 3, 4, 5], function (item) {
-		return item > 5;
-	})
-);
+console.log(maxMinChange(arr));
