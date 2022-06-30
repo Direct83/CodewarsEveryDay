c22bc5938e5d1c5e68cc8466ae27ef897c797986
Ivan Vidutin <Forumuse@yandex.ru>
2022-06-30 09:28:18 +0500

    30062022

--- code/codewars.js
+++ code/codewars.js
@@ -1,3 +1,17 @@
-const fireFight = (s) => s.replace(/Fire/g, '~~');
+const mostFrequentItemCount = (collection) =>
+	Math.max(
+		...Object.values(
+			collection.reduce(
+				(acc, el) => {
+					acc[el] = ++acc[el] || 1;
+					return acc;
+				},
+				{ 0: 0 }
+			)
+		)
+	);
 
-console.log(fireFight('Mast Deck Engine Water Fire'));
+console.log(
+	mostFrequentItemCount([3, -1, -1, -1, 2, 3, -1, 3, -1, 2, 4, 9, 3])
+);
+console.log(mostFrequentItemCount([]));
