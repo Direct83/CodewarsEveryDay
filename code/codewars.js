38eb7ac8e05ef75c0e9b439ff6626dfbad7527df
Ivan Vidutin <Forumuse@yandex.ru>
2022-07-03 13:21:13 +0500

    03072022

--- code/codewars.js
+++ code/codewars.js
@@ -1,19 +1,6 @@
-const arr = [
-	{ Бяк: 1 },
-	{ Аякса: 2 },
-	{ бал: 3 },
-	{ Аякса: 3 },
-	{ абажур: 4 },
-	{ Аякса: 1 },
-];
-
-console.log(
-	arr.sort((a, b) => {
-		const word1 = Object.keys(a)[0].toLowerCase();
-		const word2 = Object.keys(b)[0].toLowerCase();
-		if (word1 === word2) {
-			return Object.values(a) - Object.values(b);
-		}
-		return word1.localeCompare(word2);
-	})
-);
+const stantonMeasure = (arr) => {
+	const number = arr.filter((el) => el === 1).length;
+	return arr.filter((el) => el === number).length;
+};
+console.log(stantonMeasure([1, 4, 3, 2, 1, 2, 3, 2]));
+console.log(stantonMeasure([1, 4, 1, 2, 11, 2, 3, 1]));
--- completed-code/completedTasks.js
+++ completed-code/completedTasks.js
@@ -2807,3 +2807,10 @@
 // 		return word1.localeCompare(word2);
 // 	})
 // );
+
+// const stantonMeasure = (arr) => {
+// 	const number = arr.filter((el) => el === 1).length;
+// 	return arr.filter((el) => el === number).length;
+// };
+// console.log(stantonMeasure([1, 4, 3, 2, 1, 2, 3, 2]));
+// console.log(stantonMeasure([1, 4, 1, 2, 11, 2, 3, 1]));
