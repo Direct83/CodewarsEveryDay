3070cf191f7f6516eff54d62d352697ccd0afa39
Ivan Vidutin <Forumuse@yandex.ru>
2022-07-05 09:35:02 +0500

    05072022

--- code/codewars.js
+++ code/codewars.js
@@ -1,9 +1,14 @@
-const flatten = (array) => array.reduce((acc, el) => acc.concat(el), []);
+const extraPerfect = (n) =>
+	[...Array(Math.round(n / 2))].reduce(
+		(acc) => {
+			const lastNumber = acc[acc.length - 1];
+			if (lastNumber + 2 <= n) {
+				acc = acc.concat(lastNumber + 2);
+			}
+			return acc;
+		},
+		[1]
+	);
 
-console.log(
-	flatten([
-		[1, 2, 3],
-		['a', 'b', 'c'],
-		[1, 2, 3],
-	])
-);
+console.log(extraPerfect(3));
+console.log(extraPerfect(28));
--- completed-code/completedTasks.js
+++ completed-code/completedTasks.js
@@ -2824,3 +2824,18 @@
 // 		[1, 2, 3],
 // 	])
 // );
+
+// const extraPerfect = (n) =>
+// 	[...Array(Math.round(n / 2))].reduce(
+// 		(acc) => {
+// 			const lastNumber = acc[acc.length - 1];
+// 			if (lastNumber + 2 <= n) {
+// 				acc = acc.concat(lastNumber + 2);
+// 			}
+// 			return acc;
+// 		},
+// 		[1]
+// 	);
+
+// console.log(extraPerfect(3));
+// console.log(extraPerfect(28));
