57660e3d818bc1406a1d9df538b4d397a061729b
Ivan Vidutin <Forumuse@yandex.ru>
2022-07-09 10:25:34 +0500

    09072022

--- code/codewars.js
+++ code/codewars.js
@@ -1,6 +1,45 @@
-const solve = (arr) =>
-	[...Array(arr.sort((a, b) => a - b).length)].map((_, index) =>
-		index % 2 ? arr.shift() : arr.pop()
+const arr = [
+	{
+		firstName: 'Harry',
+		lastName: 'K.',
+		country: 'Brazil',
+		continent: 'Americas',
+		age: 22,
+		language: 'JavaScript',
+		githubAdmin: 'yes',
+	},
+	{
+		firstName: 'Kseniya',
+		lastName: 'T.',
+		country: 'Belarus',
+		continent: 'Europe',
+		age: 49,
+		language: 'Ruby',
+		githubAdmin: 'no',
+	},
+	{
+		firstName: 'Jing',
+		lastName: 'X.',
+		country: 'China',
+		continent: 'Asia',
+		age: 34,
+		language: 'JavaScript',
+		githubAdmin: 'yes',
+	},
+	{
+		firstName: 'Piotr',
+		lastName: 'B.',
+		country: 'Poland',
+		continent: 'Europe',
+		age: 128,
+		language: 'JavaScript',
+		githubAdmin: 'no',
+	},
+];
+
+const findAdmin = (list, lang) =>
+	list.filter(
+		({ language, githubAdmin }) => language === lang && githubAdmin === `yes`
 	);
 
-console.log(solve([15, 11, 10, 7, 12]));
+console.log(findAdmin(arr, 'JavaScript'));
