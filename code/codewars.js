705bbf12c20d64033f93acd593a5d44031e749f3
Ivan Vidutin <Forumuse@yandex.ru>
2022-07-11 09:33:07 +0500

    11072022

--- code/codewars.js
+++ code/codewars.js
@@ -1,1 +1,18 @@
-const getNames = (data) => data.map((item) => item.name);
+function numbersWithDigitInside(x, d) {
+	const arr = [...Array(x)].reduce(
+		(acc, _, index) =>
+			new RegExp(d).test(String(index + 1))
+				? (acc = acc.concat(index + 1))
+				: acc,
+		[]
+	);
+
+	return [
+		arr.length,
+		arr.reduce((acc, el) => acc + el, 0),
+		arr.length ? arr.reduce((acc, el) => acc * el, 1) : 0,
+	];
+}
+
+console.log(numbersWithDigitInside(11, 1));
+console.log(numbersWithDigitInside(1, 0));
--- completed-code/completedTasks.js
+++ completed-code/completedTasks.js
@@ -2914,3 +2914,22 @@
 // console.log(findAdmin(arr, 'JavaScript'));
 
 // const getNames = (data) => data.map((item) => item.name);
+
+// function numbersWithDigitInside(x, d) {
+// 	const arr = [...Array(x)].reduce(
+// 		(acc, _, index) =>
+// 			new RegExp(d).test(String(index + 1))
+// 				? (acc = acc.concat(index + 1))
+// 				: acc,
+// 		[]
+// 	);
+
+// 	return [
+// 		arr.length,
+// 		arr.reduce((acc, el) => acc + el, 0),
+// 		arr.length ? arr.reduce((acc, el) => acc * el, 1) : 0,
+// 	];
+// }
+
+// console.log(numbersWithDigitInside(11, 1));
+// console.log(numbersWithDigitInside(1, 0));
