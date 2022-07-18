2d2693b272020c9cf0df066d57c87d04fd5e2c34
Ivan Vidutin <Forumuse@yandex.ru>
2022-07-18 09:34:33 +0500

    18072022

--- code/codewars.js
+++ code/codewars.js
@@ -1,11 +1,11 @@
-function product(values) {
-	if (values === null || !values.length) {
-		return null;
-	}
-
-	return values.reduce((acc, el) => acc * el, 1);
+function infected(s) {
+	const total = s.replace(/X/g, '').length;
+	const infected = s
+		.split('X')
+		.filter((i) => i.includes(1))
+		.join('').length;
+	const percentage = (100 * infected) / total;
+	return percentage || 0;
 }
 
-console.log(product([5, 4, 1, 3, 9]));
-console.log(product([]));
-console.log(product(null));
+console.log(infected('01000000X000X011X0X'));
--- completed-code/completedTasks.js
+++ completed-code/completedTasks.js
@@ -3042,3 +3042,15 @@
 // 	list.reduce((acc, { meal }) => (acc[meal] = ++acc[meal] || 1) && acc, {});
 
 // console.log(orderFood(list1));
+
+// function infected(s) {
+// 	const total = s.replace(/X/g, '').length;
+// 	const infected = s
+// 		.split('X')
+// 		.filter((i) => i.includes(1))
+// 		.join('').length;
+// 	const percentage = (100 * infected) / total;
+// 	return percentage || 0;
+// }
+
+// console.log(infected('01000000X000X011X0X'));
