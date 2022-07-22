7bcb8c278381c26b4f2366a10648d9608e2cc6b6
Ivan Vidutin <Forumuse@yandex.ru>
2022-07-22 08:40:32 +0500

    22072022

--- code/codewars.js
+++ code/codewars.js
@@ -1,6 +1,33 @@
-const getAges = (sum, difference) =>
-	sum < 0 || difference < 0 || sum < difference
-		? null
-		: [(sum + difference) / 2, (sum - difference) / 2];
+const getMissingNumbers = (numbers) =>
+	[...new Set(numbers)]
+		.sort((a, b) => a - b)
+		.reduce((acc, el, index, arr) => {
+			const nextNumber = arr[index + 1];
 
-console.log(getAges(24, 4));
+			if (el !== nextNumber && nextNumber !== undefined) {
+				const numbers = [...Array(nextNumber - el - 1)].map(
+					(_, index) => index + 1 + el
+				);
+				acc.push(...numbers);
+			}
+
+			if (arr.length === 1 && numbers.length === 2) {
+				acc.push(el + 1);
+			}
+
+			return acc;
+		}, []);
+
+console.log(getMissingNumbers([1, 10]));
+console.log(getMissingNumbers([15, 20]));
+console.log(getMissingNumbers([4, 3, 2, 7, 8, 2, 3, 1]));
+console.log(getMissingNumbers([1, 2]));
+console.log(getMissingNumbers([1, 1]));
+
+function shorter_reverse_longer(a, b) {
+	const longer = a.length >= b.length ? a : b;
+	const shorter = a.length <= b.length ? a : b;
+
+	return `${shorter}${[...longer].reverse().join('')}${shorter}`;
+}
+console.log(shorter_reverse_longer('first', 'abcde'));
--- completed-code/completedTasks.js
+++ completed-code/completedTasks.js
@@ -3073,3 +3073,63 @@
 // 		: [(sum + difference) / 2, (sum - difference) / 2];
 
 // console.log(getAges(24, 4));
+
+// const getMissingNumbers = (numbers) =>
+// 	[...new Set(numbers)]
+// 		.sort((a, b) => a - b)
+// 		.reduce((acc, el, index, arr) => {
+// 			const nextNumber = arr[index + 1];
+
+// 			if (el !== nextNumber && nextNumber !== undefined) {
+// 				const numbers = [...Array(nextNumber - el - 1)].map(
+// 					(_, index) => index + 1 + el
+// 				);
+// 				acc.push(...numbers);
+// 			}
+
+// 			if (arr.length === 1 && numbers.length === 2) {
+// 				acc.push(el + 1);
+// 			}
+
+// 			return acc;
+// 		}, []);
+
+// console.log(getMissingNumbers([1, 10]));
+// console.log(getMissingNumbers([15, 20]));
+// console.log(getMissingNumbers([4, 3, 2, 7, 8, 2, 3, 1]));
+// console.log(getMissingNumbers([1, 2]));
+// console.log(getMissingNumbers([1, 1]));
+
+// const getMissingNumbers = (numbers) =>
+// 	[...new Set(numbers)]
+// 		.sort((a, b) => a - b)
+// 		.reduce((acc, el, index, arr) => {
+// 			const nextNumber = arr[index + 1];
+
+// 			if (el !== nextNumber && nextNumber !== undefined) {
+// 				const numbers = [...Array(nextNumber - el - 1)].map(
+// 					(_, index) => index + 1 + el
+// 				);
+// 				acc.push(...numbers);
+// 			}
+
+// 			if (arr.length === 1 && numbers.length === 2) {
+// 				acc.push(el + 1);
+// 			}
+
+// 			return acc;
+// 		}, []);
+
+// console.log(getMissingNumbers([1, 10]));
+// console.log(getMissingNumbers([15, 20]));
+// console.log(getMissingNumbers([4, 3, 2, 7, 8, 2, 3, 1]));
+// console.log(getMissingNumbers([1, 2]));
+// console.log(getMissingNumbers([1, 1]));
+
+// function shorter_reverse_longer(a, b) {
+// 	const longer = a.length >= b.length ? a : b;
+// 	const shorter = a.length <= b.length ? a : b;
+
+// 	return `${shorter}${[...longer].reverse().join('')}${shorter}`;
+// }
+// console.log(shorter_reverse_longer('first', 'abcde'));
