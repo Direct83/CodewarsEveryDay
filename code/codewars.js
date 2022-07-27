9e6fbfa01de5054f8ff19d3b3cc617d846012e90
Ivan Vidutin <Forumuse@yandex.ru>
2022-07-27 09:27:21 +0500

    27072022

--- code/codewars.js
+++ code/codewars.js
@@ -1,20 +1,17 @@
-function timeCorrect(timestring) {
-	const date = new Date();
+const createDict = (keys, values) =>
+	keys.reduce(
+		(acc, el, index) => (
+			(acc[el] =
+				values[index] === null || values[index] === undefined
+					? null
+					: values[index]),
+			acc
+		),
 
-	if (timestring == '') {
-		return timestring;
-	}
+		{}
+	);
 
-	if (!/\d\d:\d\d:\d\d/.test(timestring)) {
-		return null;
-	}
-
-	date.setUTCHours(...timestring.split(':'));
-	return date.toLocaleTimeString('en', { hour12: false });
-}
-
-console.log(timeCorrect('09:10:01'));
-console.log(timeCorrect('11:70:10'));
-console.log(timeCorrect('001122'));
-console.log(timeCorrect(''));
-console.log(timeCorrect(null));
+console.log(createDict(['a', 'b', 'c'], [1, 2, 3]));
+console.log(createDict(['a', 'b', 'c'], [1, 2, 3, 4]));
+console.log(createDict(['a', 'b', 'c', 'd'], [1, 2, 3]));
+console.log(createDict(['a', 'b', 'c', 'd', 'e'], [1, 2, 0, false, '']));
--- completed-code/completedTasks.js
+++ completed-code/completedTasks.js
@@ -3252,3 +3252,21 @@
 // console.log(timeCorrect('001122'));
 // console.log(timeCorrect(''));
 // console.log(timeCorrect(null));
+
+// const createDict = (keys, values) =>
+// 	keys.reduce(
+// 		(acc, el, index) => (
+// 			(acc[el] =
+// 				values[index] === null || values[index] === undefined
+// 					? null
+// 					: values[index]),
+// 			acc
+// 		),
+
+// 		{}
+// 	);
+
+// console.log(createDict(['a', 'b', 'c'], [1, 2, 3]));
+// console.log(createDict(['a', 'b', 'c'], [1, 2, 3, 4]));
+// console.log(createDict(['a', 'b', 'c', 'd'], [1, 2, 3]));
+// console.log(createDict(['a', 'b', 'c', 'd', 'e'], [1, 2, 0, false, '']));
