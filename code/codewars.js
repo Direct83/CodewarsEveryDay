2b43cdd093665a296144e3040fcefca424221f16
Ivan Vidutin <Forumuse@yandex.ru>
2022-08-18 09:06:06 +0500

    18082022

--- code/codewars.js
+++ code/codewars.js
@@ -1,3 +1,26 @@
-const unscrambleEggs = (word) => word.replace(/egg/g, '');
+function well(x) {
+	const ideas = x.reduce((acc, el) => {
+		el.forEach(
+			(idea) => String(idea).toLowerCase() === 'good' && (acc = acc + 1)
+		);
+		return acc;
+	}, 0);
 
-console.log(unscrambleEggs('ceggodegge heggeregge'));
+	if (ideas === 0) {
+		return 'Fail!';
+	}
+	return ideas <= 2 ? 'Publish!' : 'I smell a series!';
+}
+
+console.log(
+	well([['gOOd', 'bAd', 'BAD', 'bad', 'bad', 'GOOD'], ['bad'], ['gOOd', 'BAD']])
+);
+console.log(
+	well([
+		[
+			['bad', 8, 6],
+			['bAd', 'six', 'cheat'],
+			['bAd', 'bAd', 3, 8],
+		],
+	])
+);
--- completed-code/completedTasks.js
+++ completed-code/completedTasks.js
@@ -3394,3 +3394,30 @@
 // const unscrambleEggs = (word) => word.replace(/egg/g, '');
 
 // console.log(unscrambleEggs('ceggodegge heggeregge'));
+
+// function well(x) {
+// 	const ideas = x.reduce((acc, el) => {
+// 		el.forEach(
+// 			(idea) => String(idea).toLowerCase() === 'good' && (acc = acc + 1)
+// 		);
+// 		return acc;
+// 	}, 0);
+
+// 	if (ideas === 0) {
+// 		return 'Fail!';
+// 	}
+// 	return ideas <= 2 ? 'Publish!' : 'I smell a series!';
+// }
+
+// console.log(
+// 	well([['gOOd', 'bAd', 'BAD', 'bad', 'bad', 'GOOD'], ['bad'], ['gOOd', 'BAD']])
+// );
+// console.log(
+// 	well([
+// 		[
+// 			['bad', 8, 6],
+// 			['bAd', 'six', 'cheat'],
+// 			['bAd', 'bAd', 3, 8],
+// 		],
+// 	])
+// );
