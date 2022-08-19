4778d67371c9f6f09ea8cbf63948631d07199dee
Ivan Vidutin <Forumuse@yandex.ru>
2022-08-19 08:16:24 +0500

    19082022

--- code/codewars.js
+++ code/codewars.js
@@ -1,26 +1,3 @@
-function well(x) {
-	const ideas = x.reduce((acc, el) => {
-		el.forEach(
-			(idea) => String(idea).toLowerCase() === 'good' && (acc = acc + 1)
-		);
-		return acc;
-	}, 0);
+const collatz = (n) => (n > 1 ? 1 + collatz(n % 2 ? n * 3 + 1 : n / 2) : 1);
 
-	if (ideas === 0) {
-		return 'Fail!';
-	}
-	return ideas <= 2 ? 'Publish!' : 'I smell a series!';
-}
-
-console.log(
-	well([['gOOd', 'bAd', 'BAD', 'bad', 'bad', 'GOOD'], ['bad'], ['gOOd', 'BAD']])
-);
-console.log(
-	well([
-		[
-			['bad', 8, 6],
-			['bAd', 'six', 'cheat'],
-			['bAd', 'bAd', 3, 8],
-		],
-	])
-);
+console.log(collatz(20));
--- completed-code/completedTasks.js
+++ completed-code/completedTasks.js
@@ -3421,3 +3421,7 @@
 // 		],
 // 	])
 // );
+
+// const collatz = (n) => (n > 1 ? 1 + collatz(n % 2 ? n * 3 + 1 : n / 2) : 1);
+
+// console.log(collatz(20));
