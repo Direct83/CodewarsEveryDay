bbd4e6cfc03af4b46f0c9f77a6df00f8ce040f9c
Ivan Vidutin <Forumuse@yandex.ru>
2022-08-28 10:19:31 +0500

    28082022

--- completed-code/completedTasks.js
+++ completed-code/completedTasks.js
@@ -3468,3 +3468,16 @@
 
 // console.log(isDivisible(3, 3, 4));
 // console.log(isDivisible(12, 3, 4));
+
+// function splitTheBill(x) {
+// 	const average =
+// 		Object.values(x).reduce((a, b) => a + b, 0) / Object.values(x).length;
+// 	const values = Object.values(x).map((el) => el - average);
+// 	return Object.assign(
+// 		...Object.keys(x).map((key, index) => ({
+// 			[key]: parseFloat(values[index].toFixed(2)),
+// 		}))
+// 	);
+// }
+
+// console.log(splitTheBill({ A: 20, B: 15, C: 10 }));
