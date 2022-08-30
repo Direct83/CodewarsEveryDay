85056181bfb0b393ce28e88c04f5d4886903f930
Ivan Vidutin <Forumuse@yandex.ru>
2022-08-30 12:16:35 +0500

    30082022

--- code/codewars.js
+++ code/codewars.js
@@ -1,4 +1,13 @@
-const findDup = (arr) =>
-	arr.filter((el, index) => arr.indexOf(el) !== index)[0];
+function spacer(stringLength, textArr) {
+	const spacesLength = stringLength - textArr.join('').length;
+	const remains = spacesLength % (textArr.length - 1);
+	const resultArray = textArr.map((el, index) =>
+		index < remains ? el + ' ' : el
+	);
+	const countRepeat = spacesLength / (textArr.length - 1);
+	return resultArray.join(' '.repeat(countRepeat > 0 ? countRepeat : 1));
+}
 
-console.log(findDup([1, 3, 2, 5, 4, 5, 7, 6]));
+console.log(spacer(1, ['s', 'e']));
+console.log(spacer(20, ['this', 'is', 'example']));
+console.log(spacer(8, ['123456']));
