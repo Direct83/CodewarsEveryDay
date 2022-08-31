779a3d59078a8b2e98fa82b716880e1d07350d5f
Ivan Vidutin <Forumuse@yandex.ru>
2022-08-31 09:50:29 +0500

    31082022

--- code/codewars.js
+++ code/codewars.js
@@ -1,13 +1,6 @@
-function spacer(stringLength, textArr) {
-	const spacesLength = stringLength - textArr.join('').length;
-	const remains = spacesLength % (textArr.length - 1);
-	const resultArray = textArr.map((el, index) =>
-		index < remains ? el + ' ' : el
+const smaller = (nums) =>
+	nums.map(
+		(number, index) => nums.slice(index).filter((el) => el < number).length
 	);
-	const countRepeat = spacesLength / (textArr.length - 1);
-	return resultArray.join(' '.repeat(countRepeat > 0 ? countRepeat : 1));
-}
 
-console.log(spacer(1, ['s', 'e']));
-console.log(spacer(20, ['this', 'is', 'example']));
-console.log(spacer(8, ['123456']));
+console.log(smaller([5, 4, 3, 2, 1]));
--- completed-code/completedTasks.js
+++ completed-code/completedTasks.js
@@ -3500,3 +3500,10 @@
 // console.log(spacer(1, ['s', 'e']));
 // console.log(spacer(20, ['this', 'is', 'example']));
 // console.log(spacer(8, ['123456']));
+
+// const smaller = (nums) =>
+// 	nums.map(
+// 		(number, index) => nums.slice(index).filter((el) => el < number).length
+// 	);
+
+// console.log(smaller([5, 4, 3, 2, 1]));
