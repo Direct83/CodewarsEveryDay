23bda2fd8a3ea14a44af560bcef6bc681040ed03
Ivan Vidutin <Forumuse@yandex.ru>
2022-09-01 09:51:32 +0500

    01092022

--- code/codewars.js
+++ code/codewars.js
@@ -1,6 +1,7 @@
-const smaller = (nums) =>
-	nums.map(
-		(number, index) => nums.slice(index).filter((el) => el < number).length
-	);
+const multiples = (s1, s2, s3) =>
+	[...Array(s3 - s1)]
+		.map((_, index) => index + s1)
+		.filter((el) => !(el % s1 || el % s2));
 
-console.log(smaller([5, 4, 3, 2, 1]));
+console.log(multiples(13, 5, 800));
+// [65, 130, 195, 260, 325, 390, 455, 520, 585, 650, 715, 780]
