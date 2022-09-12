2587fcc651e9e3deb38b5ca6add0eef3ad5929f0
Ivan Vidutin <Forumuse@yandex.ru>
2022-09-12 20:45:32 +0500

    12092022

--- code/codewars.js
+++ code/codewars.js
@@ -1,21 +1,5 @@
-const peak = (arr) =>
-	arr.reduce((acc, _, index, arr) => {
-		const condition =
-			arr.slice(index + 1).reduce((a, b) => a + b, 0) ===
-			arr.slice(0, index).reduce((a, b) => a + b, 0);
-		if (condition && acc === -1) {
-			acc = index;
-		}
-		return acc;
-	}, -1);
+const countArara = (n) =>
+	`${`adak `.repeat(n / 2)}${`anane`.repeat(n % 2)}`.trim();
 
-console.log(peak([1, 2, 3, 5, 3, 2, 1]));
-console.log(peak([1, 12, 3, 3, 6, 3, 1]));
-console.log(
-	peak([
-		5, 5, 9, 10, 1, 7, 9, 3, 1, 2, 5, 6, 5, 8, 7, 6, 3, 7, 8, 7, 1, 7, 1, 4, 6,
-		8, 9, 9, 7, 1, 4, 4, 5, 1, 2, 5, 5, 6, 4, 7, 7, 2, 2, 8, 10, 3, 5, 5, 9, 10,
-		1, 7, 9, 3, 1, 2, 5, 6, 5, 8, 7, 6, 3, 7, 8, 7, 1, 7, 1, 4, 6, 8, 9, 9, 7,
-		1, 4, 4, 5, 1, 2, 5, 5, 6, 4, 7, 7, 2, 2, 8, 10,
-	])
-);
+console.log(countArara(1));
+console.log(countArara(3));
--- completed-code/completedTasks.js
+++ completed-code/completedTasks.js
@@ -3565,3 +3565,9 @@
 // 		}
 // 		return acc;
 // 	}, -1);
+
+// const countArara = (n) =>
+// 	`${`adak `.repeat(n / 2)}${`anane`.repeat(n % 2)}`.trim();
+
+// console.log(countArara(1));
+// console.log(countArara(3));
